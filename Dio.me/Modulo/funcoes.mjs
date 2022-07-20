c7f2c1d2aeac1233ab8c85df7a04e047732e6c80
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-07-20 19:37:59 -0300

    aula de funções iniciada

--- Dio.me/Modulo/funcoes.mjs
+++ Dio.me/Modulo/funcoes.mjs
@@ -36,4 +36,81 @@ function mostreHobby (nome, hobby) {
 }
 default export { mostreHobby }
 
+Função comun
+Estrutura inicial de uma function.
+Definição comun de uma função
+functon nome(parametros) {
+
+}
+* Variaveis criadas dentro de uma função, apenas podera ser usada dentro dela.
+
+*Return - Quando o return é invocado, a função para de ser executada e retorna o 
+que vier apos ser invocado.
+Ex: 
+function nome(parametros) {
+    // instruções
+    return // valor de retorno 'a função retorna o que estiver aqui'
+}
+
+Função anonima:
+São funções que representam expressões - uma variavel pode armazenar uma função.
+Ex:
+const soma = function (a, b) { 
+    return a + b
+}
+soma(1, 2) // 3
+soma(5, 4) // 9
+
+Função autoinvocavél:
+IIFE (Immediately Invoked Function Experssion)
+
+Uma função anonima entre parenteses, seguida por outro par de
+parenteses, que representa sua chamada.
+Ex: Função sem receber parametro retornando valor da variavel
+(
+    function() {
+        let name = "Digital Innovation One"
+        return name
+    }
+)()
+// "Digital Innovation One"
+
+Ex: Função utilizada com parametros ou armazenada em uma variavel
+Com parametros:
+(
+    function(a, b) {
+        return a + b
+    }
+)(1, 2)
+// 3
+
+Armazenando função em variavel
+
+const soma3 = (
+    funtion () {
+        return a + b
+    }
+)(1, 2)
+console.log(soma3) // 3
+
+
+Callbacks
+É uma função passada com argumento para outra.
+Utilizando callbacks, você tem maior controle da ordem de chamadas
+Ex:
+const calc = funtion (operacao, num1, num2) {
+    return operacao(num1, num2)
+}
+const soma = funtion (num1, num2) {
+    retunr num1 + num2
+}
+const sub = function (num1, num2) {
+    return num1 - num2
+}
+const resultSoma = calc(soma, 1, 2)
+const resultSub = calc(sub, 1, 2)
+
+console.log(resultSoma) // 3
+console.log(resultSub) // -1
+
 */
