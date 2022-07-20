7454cf424b71b3a281cf27b8b33ddfd4817c59d0
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-07-20 20:28:46 -0300

    exe05 criado

---
+++ Curso em Video/Aula 01/exe05.js
@@ -0,0 +1,22 @@
+/*
+Auto-atribuição:
+var n = 3
+n = n + 5
+n = n - 2
+n = n * 2
+n = 10 / n
+n = n % 2
+n = n ** 2
+simplificado:
+n += 3
+n -= 5
+
+incremento
+faz o incremento apos
+x = x + 1 // x++
+x = x - 1 // x--
+
+incremento antes
+++x // x = x + 1
+--x // x = x - 1
+*/
