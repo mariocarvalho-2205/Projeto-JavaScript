01a9555460533b9938c05af795e22eb485dbb4ae
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-07-20 22:23:20 -0300

    operadores aritmeticos

--- Curso em Video/Aula 01/exe05.js
+++ Curso em Video/Aula 01/exe05.js
@@ -19,4 +19,25 @@ x = x - 1 // x--
 incremento antes
 ++x // x = x + 1
 --x // x = x - 1
+operadores relacionais de identidade
+= // recebe
+== igual a outro sem analisar o tipo
+=== compara valor e tipo operador de identidade ou igualdeda restrita
+
+operadores logicos
+na ordem de execução
+1 - operadores aritmeticos
+2 - operadores relacionais
+3 - operadores logicos
+    1 - ! negaçao o sim vira nao
+    2 - && conjunção equivalente ao e
+    3 - || disjunção equivalente as ou
+
+
+operador ternario:
+e quando tem os operadores ?: na mesma expressao
+e formado por 3 blocos 
+o primeiro é o teste, segundo e o bloco true, e por ultimo o bloco false 
+Ex:
+var resultado = media > = 7 ? 'aprovado' : 'reprovado'
 */
