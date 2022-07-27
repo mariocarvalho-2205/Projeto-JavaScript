95f5ae9ab4259472e73ba9580e1429e5622831d3
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-07-27 20:19:08 -0300

    Mais sobre String

--- Udemy/mais sobre string.js
+++ Udemy/mais sobre string.js
@@ -1,6 +1,6 @@
 // metodos para string
 
-let umaString = 'Uma texto'  // para usar aspas duplas dentro de aspas dupla e preciso 
+let umaString = 'Um texto completo'  // para usar aspas duplas dentro de aspas dupla e preciso 
 // escapar o texto usando a barra invertida "um \"texto"" 
 // para exibir a barra invertida usa-se duas barras "um \\texto"
 
@@ -18,5 +18,23 @@ console.log(umaString.concat(' ', 'em', ' lindo dia'))  // concatena as strings
 console.log(umaString + ' em um lindo dia')
 console.log(`${umaString} em um lindo dia`)
 
+Localizando trecho do da string:
+console.log(umaString.indexOf('texto')) // esse metodo localiza o indice que inicia a string
+
+lastIndexOf // faz a mesma coisa que o indexOf, porem, começando do final
+
+console.log(umaString.match(/[a-z]/g))  // Expressoes regulares - encontra um array usando o g
+console.log(umaString.match(/[a-z]/))  // mostra as caracteristicas da variavel
+
 
  */
+
+let str = 'um texto'
+console.log(str.concat(' em um lindo dia', ' caraca'))
+console.log(`${str} em um lindo dia`)
+console.log(umaString.indexOf('texto'))
+console.log(umaString.indexOf('o', 3))
+console.log(umaString.lastIndexOf('m', 3))
+console.log(umaString.match(/[a-z]/))
+console.log(umaString.search(/[a-z]/g))  // usando o x ele retorna o indice
+console.log(umaString.search(/x/))
