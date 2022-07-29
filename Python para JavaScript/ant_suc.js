d8ba9606718eee111d0b977c42b5d63f8a1620db
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-07-28 22:45:32 -0300

    python to JS

---
+++ Python para JavaScript/ant_suc.js
@@ -0,0 +1,5 @@
+const num = prompt('Digite um numero: ')
+
+document.body.innerHTML = `Você digitou o numero ${num} <br />`
+document.body.innerHTML += `O Seu antecessor é ${num} <br />`
+document.body.innerHTML += `O seu sucessor é ${num} <br />`
