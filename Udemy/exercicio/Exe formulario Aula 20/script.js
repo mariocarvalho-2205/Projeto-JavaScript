40a0f30e3d3198a454d169d7e29cd00beabc72bd
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-08-05 21:13:15 -0300

    exercicio

--- Udemy/exercicio/Exe formulario Aula 20/script.js
+++ Udemy/exercicio/Exe formulario Aula 20/script.js
@@ -1,9 +1,10 @@
 function meuEscopo () {
-    const form = document.querySelector('.formulario'); // acessando class usa o .nome_class
-
+    const form = document.querySelector('.formulario') // acessando class usa o .nome_class
+    const resultado = document.querySelector('.resultado')
+    const pessoas = []
     /*form.onsubmit = function (evento) {
         evento.preventDefault()  // evita que o formulario atualiza a pagina e perca os dados armazenados
-
+        
         alert(1)
         console.log('foi enviado')
     };*/
@@ -13,11 +14,18 @@ function meuEscopo () {
         const nome = form.querySelector('.nome')
         const sobrenome = form.querySelector('.sobrenome')
         const idade = form.querySelector('.idade')
-        const altura = form.querySelector('altura')
-        console.log(nome, sobrenome, idade, altura)
+        const altura = form.querySelector('.altura')
         /*console.log(`form nao foi enviado ${contador}`)
         contador++*/
-    }
+        pessoas.push({
+            nome: nome.value, 
+            sobrenome: sobrenome.value,
+            idade: idade.value,
+            altura: altura.value
+        })
+        console.log(pessoas)
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${idade.value} ${altura.value}</p>`
+    }   
     form.addEventListener('submit', recebeEventoForm)
 }
 meuEscopo()
