f200b1388f68fafdd92192736be39763b411a363
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-08-06 22:43:30 -0300

    exercicio1

--- Curso em Video/Exercicios/exe_formulario/script.js
+++ Curso em Video/Exercicios/exe_formulario/script.js
@@ -1,1 +1,22 @@
-alert('Bem vindo')
+function carregar () {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()  // peaga a hora do sistema
+    //var hora = data.getHours()  // 
+    var hora = 8
+    msg.innerHTML += `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        // bom dia
+        img.src = './assets/manha.jpg'  // acessa a imagem dentro da pasta
+        document.body.style.background = 'blue'  // para usar cor hexadecimar '# antes do codigo'
+    } else if ( hora >= 12 && hora < 18 ) {
+        // boa tarde
+        img.src = './assets/tarde.jpg'
+        document.body.style.background = 'red'
+    } else {
+        // boa noite
+        img.src = './assets/noite.jpg'
+        document.body.style.background = 'gray'
+    }
+}
+    
