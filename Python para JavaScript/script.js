cf4f58ff97ea5c4a3cb1b88a7f6e1dc2dcc43f1a
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-08-09 21:52:35 -0300

    dobro_triplo

--- Python para JavaScript/script.js
+++ Python para JavaScript/script.js
@@ -5,8 +5,10 @@ function calculo() {
     let temp = Number(num.value)
     let dobro = temp * 2
     let triplo = temp * 3 
+    let raiz = Math.sqrt(temp)
     resultado.innerHTML = `<p>O dobro de <strong>${temp}</strong> é <strong>${dobro}<strong><br /><p/>`
     resultado.innerHTML += `<p>O triplo de <strong>${temp}</strong> é <strong>${triplo}</strong></p>`
+    resultado.innerHTML += `<p>A Raiz de <strong>${temp}</strong> é <strong>${raiz.toFixed(2)}</strong></p>`
     resultado.style.backgroundColor = 'gray'
     resultado.style.borderRadius = '10px'
 }
