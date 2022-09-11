09a1b4d2b5518f248908bc51d9e8ee84681c5f88
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-09-10 22:03:59 -0300

    novos exercicios a concluir

---
+++ Python para JavaScript/Separando Unidades/assets/js/main.js
@@ -0,0 +1,15 @@
+const num = '4876'
+const numbe = 4876
+console.log(num[3])
+console.log(num[2])
+console.log(num[1])
+console.log(num[0])
+let u = parseInt(num % 1000)
+let d = (parseInt(num % 100)).toFixed()
+let c = parseInt(numbe / 10) % 10
+let m = parseInt(numbe % 1)
+
+console.log(u)
+console.log(d)
+console.log(num[1])
+console.log(num[0])
