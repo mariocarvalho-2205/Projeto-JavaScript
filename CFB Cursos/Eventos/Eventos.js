da390a2bf5ef3ace83b9ecffab8dd620ac68a027
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-09-14 20:46:34 -0300

    event

--- CFB Cursos/Eventos/Eventos.js
+++ CFB Cursos/Eventos/Eventos.js
@@ -1,23 +1,43 @@
-function mudaCor (obj, cor) {
-    obj.style.backgroundColor = cor
+const btn = document.getElementById("btn");
+const test = document.querySelector('.test')
+
+function mudaCor(obj, cor) {
+  obj.style.backgroundColor = cor;
+}
+function mudaCor2(obj, cor) {
+  obj.style.backgroundColor = cor;
 }
 
-function mudaPos (obj) {
-    obj.style.position = 'absolute'
-    obj.style.left = Math.random() * 500 + 'px'
-    obj.style.top = Math.random() * 500 + 'px'
+function mudaPos(obj) {
+  obj.style.position = "absolute";
+  obj.style.left = Math.random() * 500 + "px";
+  obj.style.top = Math.random() * 500 + "px";
 }
 
-function hora () {
-    let data = new Date()
-    let hora = data.getHours()
-    let text = ''
-    if (hora < 12) {
-        text = 'Bom dia'
-    } else if (hora > 12) {
-        text = 'Boa tarde'
-    } else {
-        text = 'boa noite'
-    }
-    alert(`Olá, ${text}`)
+function hora() {
+  let data = new Date();
+  let hora = data.getHours();
+  let text = "";
+  if (hora < 12) {
+    text = "Bom dia";
+  } else if (hora > 12) {
+    text = "Boa tarde";
+  } else {
+    text = "boa noite";
+  }
+  alert(`Olá, ${text}`);
+}
+function cor(obj, cor) {
+  obj.style.color = cor;
+}
+
+function mudou() {
+  btn.style.backgroundColor = "yellow";
+  test.style.backgroundColor = "pink";
+
+}
+function mudou2() {
+  btn.style.backgroundColor = "black"
+  btn.style.color = "white";
+  
 }
