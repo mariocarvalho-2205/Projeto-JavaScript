809f4650af325fdaa848ddbed228fcc0203ac976
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-09-18 11:45:17 -0300

    Escopo Lexico

---
+++ Udemy/Escopo Lexico.js
@@ -0,0 +1,11 @@
+// Escopo Lexico
+/* é quando a função tem uma variavel declarada dentro ou ela busca a variavel fora do escopo da função
+Ex:
+cosnt nome = "Mario"
+function pessoa () {
+    se a variavel for criada dentro do esxopo da função ira retornar o valor criado dentro do escopo
+    -*-*-*-*-*-*-*
+    console.log(nome)  // chamando a variavel criada fora do escopo da função 
+    retornara // Mario
+}
+*/ 
