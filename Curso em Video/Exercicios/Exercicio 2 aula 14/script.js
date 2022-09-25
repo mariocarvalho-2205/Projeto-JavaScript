1d674fcec5cdf4c704863d8e4b052f642ae70442
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-09-25 13:36:38 -0300

    atualizadando

--- Curso em Video/Exercicios/Exercicio 2 aula 14/script.js
+++ Curso em Video/Exercicios/Exercicio 2 aula 14/script.js
@@ -1,37 +1,34 @@
 
 let radioBtns = document.querySelectorAll("input[name='sinais']")
 let res = document.getElementById('res')
+var radioBtn
 
 let findSelect = () => {
     let selected = document.querySelector("input[name='sinais']:checked").value;
     console.log(selected)
+    
 }
 
 radioBtns.forEach(radioBtn => {
     radioBtn.addEventListener("change", findSelect)
+      
 })
-
-
-
 
 /*
 btn.addEventListener('submit', (e) => {
     e.preventDefault()
-     
-    
-    //var num = Number(document.getElementById('num').value)
-    
-    
-    
+    var num = Number(document.getElementById('num').value)
+
     function soma () {
-        for (let i = 1; i <= 10; i++) {
-            res.innerHTML += `${num} + ${i} = ${num + i}<br />`
-            
-        }
+        if (findSelect === 'sum') {
+            for (let i = 1; i <= 10; i++) {
+                res.innerHTML += `${num} + ${i} = ${num + i}<br />`   
+            }
+        }    
     }    
     soma()
 
-    
+
 }) */
 
 
