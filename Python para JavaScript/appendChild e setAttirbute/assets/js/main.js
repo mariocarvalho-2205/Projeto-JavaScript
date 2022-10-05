06a002b393377290208042d50f1325a340f18a22
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-10-05 20:51:03 -0300

    setAttribute and createElement

--- Python para JavaScript/appendChild e setAttirbute/assets/js/main.js
+++ Python para JavaScript/appendChild e setAttirbute/assets/js/main.js
@@ -1,15 +1,42 @@
-const container = document.querySelector('.container')
-const input = document.createElement('input')
-const p = document.createElement("p")
-const div = document.createElement("div")
-const label = document.createElement('label')
+// selecionando o body
+const body = document.querySelector('body')
 
-container.appendChild(div)
+// criando os elementos
+const createSection = document.createElement('section')
+const createH1 = document.createElement('h1')
+const createP = document.createElement('p')
+const createH2 = document.createElement('h2')
 
-div.appendChild(p)
-div.appendChild(input)
-div.appendChild(label)
-input.setAttribute('id', 'textInput')
-input.setAttribute('type', 'text')
-p.innerText = 'paragrafo'
-p.setAttribute('id', 'paragrafo')
+// selecionando elemento filho para adicionar filhos ao filho
+const section = document.querySelector('.container')
+
+
+// adicionando elemento filho no elemento pai
+body.appendChild(createSection)
+createSection.appendChild(createH1)
+
+
+// adicionando elementos com insertAdjacentElement(local, elemento)
+createSection.insertAdjacentElement('beforeend', createP)
+createH1.insertAdjacentElement('afterend', createH2)
+
+// adicionando atributos
+createSection.classList = 'container'  // adiciona classes
+createSection.setAttribute('id', 'section')  // adiciona os demais atributos
+createH1.setAttribute('id', 'titulo-h1')
+createH2.setAttribute('id', 'titulo-h2')
+
+
+
+// adicionando texto aos elementos
+createH1.textContent = 'Titulo da pagina'
+createP.textContent = 'meu paragrafo'
+createH2.innerText = 'Subtitulo da pagina'
+
+
+
+
+
+
+
+console.log(body)
