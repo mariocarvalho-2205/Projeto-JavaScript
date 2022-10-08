b6ade22ec1a2c28bfafdfdbf64873944f444d8fc
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-10-08 17:56:17 -0300

    atualizando

---
+++ Python para JavaScript/Problema Dardo Udemy Algoritmos/assets/js/main.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('#form')
+const dardos = [document.querySelectorAll('.distancia')]
+//const dardo2 = document.querySelector('#distancia-2')
+//const dardo3 = document.querySelector('#distancia-3')
+let dar
+
+//let dar2 = Number(dardo2.value)
+//let dar3 = Number(dardo3.value)
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    console.log(dar);
+})
+
