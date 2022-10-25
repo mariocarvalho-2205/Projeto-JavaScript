dc70f105b164ef74bdd2905eb96e8c0b376b4ab9
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-10-24 22:08:58 -0300

    function Factory

--- Udemy/Função Fabrica (factory Function).js
+++ Udemy/Função Fabrica (factory Function).js
@@ -23,3 +23,44 @@ const p1 = criaPessoa('mario', 'carvalho', 47, 55, 1.6)
 console.log(p1.nome, p1.media(8, 9))
 console.log(p1.sobrenome)
 console.log(p1.imc())
+
+function criaCelular (marca, modelo, tamanho, tela) {
+    return {
+        marca,
+        modelo,
+        tamanho,
+        tela,
+        ligar(nome) {
+            return nome + 'ligado'
+        }
+    }
+}
+
+const cel1 = criaCelular('xiaomi', 'redmi Note10', 6.0, 5.5)
+
+console.log(cel1.modelo, cel1.ligar('power'))
+
+function criaCArro (marca, modelo, anoFabricacao, combustivel) {
+    return {
+        marca,
+        modelo,
+        anoFabricacao,
+        combustivel,
+        aprovado() {
+            if (anoFabricacao < 2000) {
+                return ' esta velho'
+            } 
+            return ' esta novo'
+        },
+
+        consumo() {
+            if (combustivel === 'gasolina') {
+                return 'o consumo é baixo'
+            }
+            return 'o consumo é alto'
+        }
+    }
+}
+
+const carro = criaCArro('audi', 'rs8', 2001, 'gasolina')
+console.log(carro.marca, carro.modelo, carro.anoFabricacao, carro.consumo(), carro.aprovado())
