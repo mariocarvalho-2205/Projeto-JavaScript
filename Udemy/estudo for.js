86a95fb8ca0025cb0bb75106d55a4d9314bc68ec
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-11-23 21:43:10 -0300

    estudando for

--- Udemy/estudo for.js
+++ Udemy/estudo for.js
@@ -1,5 +1,44 @@
-let stri = "mario sergio carvalho"
+let stri = "mario sergio"
 let arra = stri.split(' ')
 
+let cont = 1
+
 //console.log(stri)
-//console.log(arra)
+
+/*for (i of arra) {
+    for (j of i) {
+        if (j !== j + 1) {
+            cont += 1
+        }
+    }
+}*/
+
+for (i in arra) {
+    console.log(arra[i])
+    for (j of arra[i]) {
+        if (j == arra[i]) {
+            console.log(j)
+        } else {
+            console.log('nao')
+        }
+            
+        cont += 1    
+    }
+        
+}
+
+function duplicateCount(string) {
+    const map = [...string.toLowerCase()].reduce(
+      (map, char) => ({
+        ...map,
+        [char]: (map[char] || 0) + 1
+      }),
+      {}
+    );
+  
+    return Object.values(map).filter((count) => count > 1).length;
+  }
+  
+  console.log(duplicateCount('mario sergio')); // 2
+  console.log(duplicateCount('aABCbc33zzzzzqw')); // 5
+
