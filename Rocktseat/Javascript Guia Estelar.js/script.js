1b75690a0fdb9fa17824f59b7eba6e3a92d5b6da
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-12-20 16:39:28 -0300

    Guia Estelar javascript

---
+++ Rocktseat/Javascript Guia Estelar.js/script.js
@@ -0,0 +1,34 @@
+/* 
+
+
+? Conforme o ECMAScript Standard (o padrão do orgão que padroniza o JavaScript) temos 9 tipos de dados organizados em 4 categorias:
+
+
+! Datatypes
+    ? Primitive, Primitive Value, Structural, Structural Primitive. 
+
+* Os primitivos são: 
+* String, 
+* Number, 
+* Boolean, 
+* Undefined, 
+* Symbol, 
+* BigInt. 
+
+? Os estruturais são: 
+* Object
+    * array
+    * map
+    * set
+    * date
+    * ...
+* Function 
+
+? Os primitivos estruturais / Structural Root Primitive
+
+* - consiste apenas no dado null
+
+ */
+
+console.log({nome: 'jose', idade: 48},['mario', 'joao'])
+console.table({nome: 'jose', idade: 48}, ['mario', 'joao'])
