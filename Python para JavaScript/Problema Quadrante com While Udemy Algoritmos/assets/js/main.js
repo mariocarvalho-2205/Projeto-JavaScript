dc61388e9c208bc423a5c809564dd1603222dda8
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-12-28 22:10:35 -0300

    exercicios quadrante com while

--- Python para JavaScript/Problema Quadrante com While Udemy Algoritmos/assets/js/main.js
+++ Python para JavaScript/Problema Quadrante com While Udemy Algoritmos/assets/js/main.js
@@ -6,20 +6,21 @@ function quadrante ( ) {
         /*if (typeof valueX === 'number' && typeof valueY === 'number') {
             console.log('sao numeros')
         }*/
-        while (valueY !== '' && valueX !== '') {
-            console.log('cheio', valueX, valueY)
 
-            if (valueX === 0 && valueY === 0) alert('Origem')
-            if (valueX > 0 && valueY > 0) alert('Q1')
-            if (valueX > 0 && valueY < 0) alert('Q2')
-            if (valueX < 0 && valueY < 0) alert('Q3')
-            if (valueX < 0 && valueY > 0) alert('Q4')
-            if (valueX > 0 && valueY === 0) alert('Eixo X')
-            if (valueX === 0 && valueY > 0) alert('Eixo Y')
+    while (valueY !== '' && valueX !== '') {
+        console.log('cheio', valueX, valueY)
 
-            valueX = prompt('Digite um valor para o eixo X: ')
-            valueY = prompt('Digite um valor para o eixo Y: ')
-        }
+        if (valueX === 0 && valueY === 0) alert('Origem')
+        if (valueX > 0 && valueY > 0) alert('Q1')
+        if (valueX > 0 && valueY < 0) alert('Q2')
+        if (valueX < 0 && valueY < 0) alert('Q3')
+        if (valueX < 0 && valueY > 0) alert('Q4')
+        if (valueX > 0 && valueY === 0) alert('Eixo X')
+        if (valueX === 0 && valueY > 0) alert('Eixo Y')
+
+        valueX = prompt('Digite um valor para o eixo X: ')
+        valueY = prompt('Digite um valor para o eixo Y: ')
+    }
     console.log('vazio')
 }
 quadrante()
