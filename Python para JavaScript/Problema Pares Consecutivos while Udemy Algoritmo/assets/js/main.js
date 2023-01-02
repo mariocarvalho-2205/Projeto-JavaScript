94c24d17ea6281c01494071fed707f668043de51
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-01-01 21:41:46 -0300

    exercicios Pares Consecutivos com while finalizado

--- Python para JavaScript/Problema Pares Consecutivos while Udemy Algoritmo/assets/js/main.js
+++ Python para JavaScript/Problema Pares Consecutivos while Udemy Algoritmo/assets/js/main.js
@@ -1,4 +1,4 @@
-let int = Number(prompt('Digite um numero inteiro! Ou "0" para Sair!!!'))
+/*let int = Number(prompt('Digite um numero inteiro! Ou "0" para Sair!!!'))
 let soma = 0
 let total = 0
 if (int === 0) alert('Programa Finalizado!!!')
@@ -25,6 +25,18 @@ while (int !== 0 && int > 0){
     total = 0
     int = Number(prompt('Digite um numero inteiro! Ou "0" para Sair!!!'))
     if (int === 0) alert('Programa Finalizado!!!')
-}
+}*/
 
+// solução
+let x = Number(prompt('digite um numero'))
+let soma;
+
+while (x !== 0 && x > 0) {
+    if (x % 2 !== 0) {
+        x += 1
+    }
+    soma = 5 * x + 20
+    console.log(soma)
+    x = Number(prompt('digite um numero'))
+}
 
