609c45e1c5e804b51bc4d00fc6e63441352da8c5
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-01-02 21:45:21 -0300

    problema soma impares Udemy Algoritmos

---
+++ Python para JavaScript/Problema Soma Impares While Udemy Algoritmos/assets/js/main.js
@@ -0,0 +1,23 @@
+let intX = Number(prompt('Digite um valor para X:'))
+let intY = Number(prompt('Digite um valor para Y:'))
+let troca = 0
+let cont = 0
+let soma = 0
+
+if (intX > intY) {
+    troca = intX
+    intX = intY
+    intY = troca
+}
+cont = intX
+
+do {
+    if (cont % 2 !== 0) {
+        soma += cont
+        console.log(soma, cont)
+    }
+
+
+    cont += 1
+} while (cont <= intY - 1)
+
