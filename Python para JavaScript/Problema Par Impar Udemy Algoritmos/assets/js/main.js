402afb71625f6f87c9d10ba48a80c4cc52f8c6f0
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-01-07 13:42:02 -0300

    par impar

--- Python para JavaScript/Problema Par Impar Udemy Algoritmos/assets/js/main.js
+++ Python para JavaScript/Problema Par Impar Udemy Algoritmos/assets/js/main.js
@@ -1,7 +1,10 @@
 let qtdDezenas = Number(prompt('Quantos numeros você vai digitar? '))
-let dezenas;
-let imparNeg = dezenas % 2 !== 0 && dezenas < 0
-let parNeg = dezenas  % 2 === 0 && dezenas < 0
+let dezenas = 0
+let imparNeg = dezenas % 2 !== 0 //&& dezenas < 0
+let imparPos = dezenas % 2 !== 0 //&& dezenas > 0
+let parNeg = dezenas  % 2 === 0 //&& dezenas < 0
+let parPos = dezenas % 2 === 0 //&& dezenas < 0
+
 let msg;
 for (let i = 1; i <= qtdDezenas; i++) {
     dezenas = Number(prompt(`Digite o ${i}º numero: `))
@@ -11,6 +14,10 @@ for (let i = 1; i <= qtdDezenas; i++) {
     } else if (parNeg) {
         msg = `${dezenas} é PAR NEGATIVO`
         console.log(msg)
+    } else if (imparPos) {
+        msg = `${dezenas} é IMPAR POSITIVO`
+    } else if (parPos) {
+        msg = `${dezenas} é PAR POSITIVO`
     }
     console.log(i, dezenas)
 }
