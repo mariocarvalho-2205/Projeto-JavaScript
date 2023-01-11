b7fb8a6b5a1795cea9471d64de457a91fcefc6fc
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-01-10 22:28:01 -0300

    exercicios array altura finalizado

--- Python para JavaScript/Problema Array Alturas Udemy Algoritmos/assets/js/main.js
+++ Python para JavaScript/Problema Array Alturas Udemy Algoritmos/assets/js/main.js
@@ -1,7 +1,41 @@
-//let numberPerson = Number(prompt('Quantas pessoas serão digitadas? '))
+let numberPerson = Number(prompt('Quantas pessoas serão digitadas? '))
 
+function personColect (num) {
+    let personName = []
+    let personIdade = []
+    let personAltura = []
 
+    for (let i = 0; i < num; i++) {
+        alert(`Dados da ${i + 1}ª pessoa.`)
+        let name = prompt('Nome: ')
+        let idade = Number(prompt('Idade: '))
+        let altura = Number(prompt('Altura: '))
+        personName.push(name)
+        personIdade.push(idade)
+        personAltura.push(altura)
 
+    }
+    dados(personName, personIdade, personAltura)
+    media(personAltura)
+    return
+}
+
+function dados (name, idade, altura) {
+    for (let i = 0; i < name.length; i++) {
+        console.log(name[i], idade[i], altura[i].toFixed(2))
+    }
+}
+
+function media (altura) {
+
+    let qtdIdade = altura.length
+    let mediaIdade = altura.reduce((total, value) => {
+        return (total + value) /  qtdIdade
+    })
+
+    console.log(mediaIdade, qtdIdade)
+}
+personColect(numberPerson)
 
 
 
