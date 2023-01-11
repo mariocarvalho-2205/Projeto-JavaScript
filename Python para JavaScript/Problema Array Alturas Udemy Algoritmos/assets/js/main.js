5c1a38b26504cfcd312df9c16304cdbaa4ed4adb
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-01-10 21:21:09 -0300

    Problema Array altura finalizado

---
+++ Python para JavaScript/Problema Array Alturas Udemy Algoritmos/assets/js/main.js
@@ -0,0 +1,23 @@
+//let numberPerson = Number(prompt('Quantas pessoas serão digitadas? '))
+
+
+
+
+
+
+
+/*
+function fraseBomDia (name, lastName) {
+    const frases = []
+
+    for (let i = 0; i < name.length; i++) {
+        const frase = `Olá ${name[i]} ${lastName[i]} bom dia`
+        frases.push(frase)
+    }
+    return frases.join(',  ')  // Transforma a saida em string
+}
+
+const nameA = ['Mário']
+const lastNameB = ['Carvalho']
+
+console.log(fraseBomDia(nameA, lastNameB))*/
