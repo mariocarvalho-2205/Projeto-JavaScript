8de0c65fe32db1bbce76344621912e5f59d4e3df
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-01-27 14:37:37 -0300

    Problema Array mais velho faltando finaliizar

--- Python para JavaScript/Problema Array Mais Velho Udemy Algoritmo/assets/js/main.js
+++ Python para JavaScript/Problema Array Mais Velho Udemy Algoritmo/assets/js/main.js
@@ -15,14 +15,28 @@ function colect (num) {
 
 // criaPerson - cria usuario e adiciona array de objetos
 function criaPerson (name, idade) {
-    //let person = {name: name, idade: idade} ! pode ser criado objeto
+
+    //let person = {name: name, idade: idade} //! pode ser criado objeto
     // dentro da função e mandar a variavel
+
+    //arrayPerson.push(/*{name, idade}*/person)
+
     return arrayPerson.push({
         name,
         idade
     })
 }
 
-
 colect(qtdPerson)
+
+/*const maisVelha = arrayPerson.reduce((acu, value) => {
+    if (acu.idade > value.idade) {
+        return `A pessoa mais velha é ${acu.name}`;
+    }  else {
+        return `A pessoa mais velha é ${value.name}`;
+    }
+
+})
+console.log(maisVelha)*/
+
 console.log(arrayPerson)
