544e6b18d52496cd6aeb4a61db56eab3f0cb02e9
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-02-02 16:11:48 -0300

    Problema Array comerciante finalizado

--- Python para JavaScript/Problema Array Comerciante Udemy Algoritmos/assets/js/main.js
+++ Python para JavaScript/Problema Array Comerciante Udemy Algoritmos/assets/js/main.js
@@ -8,7 +8,7 @@ function inputProducts (qtd) {
     let compra = []
     let venda = []
     for (let i = 1; i <= qtd; i++) {
-        alert(`Produto ${i}:`)
+        //alert(`Produto ${i}:`)
         nome = prompt(`Nome ${i}`)
         priceCompra = Number(prompt(`Preço de compra ${i}`))
         priceVenda = Number(prompt(`Preço de venda ${i}`))
@@ -16,7 +16,52 @@ function inputProducts (qtd) {
         compra.push(priceCompra)
         venda.push(priceVenda)
     }
+
+    lucro(compra,venda)
     console.log(products, compra, venda)
 }
 
+function totalCompraVenda (compra, venda) {
+    let totalCompra = compra.reduce((total, item) => total + item)
+    let totalVenda = venda.reduce((total, item) => total + item)
+    let lucroTotal = totalVenda - totalCompra
+    console.log(`
+        Total de compra R$ ${totalCompra.toFixed(2)}
+        Total de venda R$ ${totalVenda.toFixed(2)}
+        Lucro Total: R$ ${lucroTotal.toFixed(2)}
+    `)
+
+}
+
+function lucro (compra, venda) {
+    let lucroProdutos = []
+    let lucroMenorQueDez = 0
+    let lucroEntreDezeVinte = 0
+    let lucroMaiorQueVinte = 0
+
+    for (let i = 0; i < compra.length; i++) {
+        lucroProdutos.push(venda[i] - compra[i])
+    }
+
+    // lucro * 100 / preço de compra
+    for (ind in lucroProdutos){
+        if ((lucroProdutos[ind] * 100) / compra[ind] < 10) {
+            lucroMenorQueDez += 1
+        }
+        if ((lucroProdutos[ind] * 100) / compra[ind] >= 10 && (lucroProdutos[ind] * 100) / compra[ind] <= 20) {
+            lucroEntreDezeVinte += 1
+        }
+        if ((lucroProdutos[ind] * 100) / compra[ind] > 20) {
+            lucroMaiorQueVinte += 1
+        }
+    }
+
+    console.log(`
+        Lucro abaixo de 10%: ${lucroMenorQueDez}
+        Lucro entre 10% e 20%: ${lucroEntreDezeVinte}
+        Lucro acima de 20%: ${lucroMaiorQueVinte}
+    `)
+    totalCompraVenda(compra, venda)
+
+}
 inputProducts(qtdProdutos)
