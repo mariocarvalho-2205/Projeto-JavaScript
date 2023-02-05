de33e1d786fe8a153556a9fbca8578184fd86396
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-02-05 20:43:36 -0300

    exercicios array bidimensional

--- teste array.js
+++ teste array.js
@@ -1,4 +1,15 @@
-let text = 'mario sergio'.split()
+/*let text = 'mario sergio'.split()
 let arra = text
 
-console.log(arra)
+console.log(arra)*/
+
+const arrayBidimensional = [[4, 6, 7], [9, 1, 3]]
+
+for (let l = 0; l < 2; l++) {
+    for (let c = 0; c < 3 ; c++){
+        console.table(`Posição [${l}, ${c}] - ${arrayBidimensional[l][c]}`)  // Posição [0, 0] - 4
+        //console.table(`Posição [${l}, ${c}] - ${arrayBidimensional[c]}`)  // Posição [0, 0] - 4,6,7
+        //console.table(`${arrayBidimensional[c]}`)  // 4,6,7
+    }
+
+}
