e9b91ea7d078ae123d11d51dca250ad55764908c
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-02-06 10:49:02 -0300

    Problema Array Matriz finalizado

--- Python para JavaScript/Problema Array Matriz Udemy Algoritmo/assets/js/main.js
+++ Python para JavaScript/Problema Array Matriz Udemy Algoritmo/assets/js/main.js
@@ -1,5 +1,5 @@
 function inputLinhasColunas () {
-    alert('Digite a quantidade de Linhas e Colunas')
+    //alert('Digite a quantidade de Linhas e Colunas')
     const inputLinhas = Number(prompt('Digite o numero de linhas:'))
     const inputColunas = Number(prompt('Digite o numero de colunas:'))
     inputMatriz(inputLinhas, inputColunas)
@@ -10,15 +10,23 @@ function inputMatriz (linhas, colunas) {
     let linha = []
     let input
     for(let i = 0; i < linhas; i++) {
-
+        linha = []
         for(let j = 0; j < colunas; j++) {
             input = Number(prompt(`Elemento [${i}, ${j}]`))
             linha.push(input)
 
         }
         arrayMatriz.push(linha)
-        console.log(arrayMatriz)
     }
+    printArray(arrayMatriz)
+}
 
+function printArray (matriz) {
+    //console.log(matriz)
+    for (let i = 0; i < matriz.length; i++) {
+        for (let j = 0; i < matriz.length; i++) {
+            console.log(`${matriz[i]}`)
+        }
+    }
 }
 inputLinhasColunas()
