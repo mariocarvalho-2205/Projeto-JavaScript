c6e14ff21878c4f419201d0af5b323d4d5828298
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-02-08 15:00:21 -0300

    Array Matriz soma matrizes em andamento

---
+++ Python para JavaScript/Problema Array Matriz Soma Matrizes Udemy Algoritmos/assets/js/main.js
@@ -0,0 +1,16 @@
+const qtdLinha = Number(prompt('Quantas linhas vao ter cada Matriz?'))
+const qtdColunas = Number(prompt('Quantas colunas vao ter cada Matriz?'))
+
+
+function criaMatrizes (linhas, colunas) {
+    let matrizA = new Array()
+    let matrizB = new Array()
+
+    for (let i = 0; i < linhas; i++) {
+        for (let j = 0; j < colunas; j++) {
+
+        }
+    }
+}
+
+criaMatrizes(qtdLinha, qtdColunas)
