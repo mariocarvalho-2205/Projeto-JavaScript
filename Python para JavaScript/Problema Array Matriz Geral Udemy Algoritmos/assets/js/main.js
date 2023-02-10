ccdb18d3e2656bd5dab0b7acd8d347714b39841b
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-02-10 11:21:42 -0300

    Array Matriz Geral iniciado

---
+++ Python para JavaScript/Problema Array Matriz Geral Udemy Algoritmos/assets/js/main.js
@@ -0,0 +1,24 @@
+/*
+a) calcular e imprimir a soma de todos os elementos positivos da matriz.
+b) fazer a leitura do índice de uma linha da matriz e, daí, imprimir todos os elementos desta linha.
+c) fazer a leitura do índice de uma coluna da matriz e, daí, imprimir todos os elementos desta coluna.
+d) imprimir os elementos da diagonal principal da matriz.
+e) alterar a matriz elevando ao quadrado todos os números negativos da mesma. Em seguida imprimir
+a matriz alterada.
+
+Ex:
+SOMA DOS POSITIVOS: 40.0
+
+Escolha uma linha: 1
+LINHA ESCOLHIDA: -2.0 3.0 5.0
+
+Escolha uma coluna: 2
+COLUNA ESCOLHIDA: 10.0 5.0 4.0
+
+DIAGONAL PRINCIPAL: 7.0 3.0 4.0
+
+MATRIZ ALTERADA:
+7.0 64.0 10.0
+4.0 3.0 5.0
+11.0 225.0 4.0
+*/
