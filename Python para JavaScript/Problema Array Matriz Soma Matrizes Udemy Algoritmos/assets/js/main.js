2bee7117786ff930b397c62adfc1c58a0dc3d23a
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-02-09 21:42:15 -0300

    exercicios array matriz soma matrizez

--- Python para JavaScript/Problema Array Matriz Soma Matrizes Udemy Algoritmos/assets/js/main.js
+++ Python para JavaScript/Problema Array Matriz Soma Matrizes Udemy Algoritmos/assets/js/main.js
@@ -1,16 +1,59 @@
-const qtdLinha = Number(prompt('Quantas linhas vao ter cada Matriz?'))
+const qtdLinhas = Number(prompt('Quantas linhas vao ter cada Matriz?'))
 const qtdColunas = Number(prompt('Quantas colunas vao ter cada Matriz?'))
+let arrayMatrizA = new Array()
+let arrayMatrizB = new Array()
 
+let linha = new Array()
+let input
 
-function criaMatrizes (linhas, colunas) {
-    let matrizA = new Array()
-    let matrizB = new Array()
+let matrizA = ((qtdLinha, qtdColunas) => {
+    console.log(`Digite os valores da matriz A:`)
 
-    for (let i = 0; i < linhas; i++) {
-        for (let j = 0; j < colunas; j++) {
+    for (let i = 0; i < qtdLinha; i++) {
+        linha = []
 
+        for (let j = 0; j < qtdColunas; j++) {
+            input = Number(prompt(`Elemento [${i}, ${j}]:`))
+            linha.push(input)
+        }
+        arrayMatrizA.push(linha)
+    }
+    return arrayMatrizA
+})
+
+let matrizB = ((qtdLinha, qtdColunas) => {
+    console.log('Digite os valores da matriz B:')
+
+    for (let i = 0; i < qtdLinha; i++) {
+        linha = []
+
+        for (let j = 0; j < qtdColunas; j++) {
+            input = Number(prompt(`Elemento [${i}, ${j}]:`))
+            linha.push(input)
+        }
+        arrayMatrizB.push(linha)
+    }
+    return arrayMatrizB
+})
+
+function criaMatrizez (qtdLinhas, qtdColunas) {
+    matrizA(qtdLinhas, qtdColunas)
+    matrizB(qtdLinhas, qtdColunas)
+
+}
+matrizC(arrayMatrizA, arrayMatrizB)
+
+function matrizC (matA, matB) {
+    let matrizC = new Array()
+    console.log(matA, matB)
+
+    console.log('Matriz Soma:')
+    for (let i = 0; i < qtdLinhas; i++) {
+        for (let j = 0; j < qtdLinhas; j++) {
+            matrizC.push(matA[i][j] + matB[i][j])
         }
     }
+    console.log(matrizC)
+    //matrizC.forEach((value) => console.log(value))
 }
-
-criaMatrizes(qtdLinha, qtdColunas)
+criaMatrizez(qtdLinhas, qtdColunas)
