11e0ea878a2813f98d1c88fb07890385de93d251
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-02-23 15:10:09 -0300

    Criando, Adicionando e manipulando elementos no DOM

--- Rocktseat/Pilotando com a DOM/introdução/script.js
+++ Rocktseat/Pilotando com a DOM/introdução/script.js
@@ -1,4 +1,5 @@
-const bod = document.querySelector('body')
+const ele = document.querySelector('header')
+const body = document.querySelector("body")
 const element = document.getElementById('title-h1')
 const elements = document.getElementsByClassName('class-p')
 const tag = document.getElementsByTagName('h1')
@@ -6,6 +7,7 @@ const textA = document.getElementById('a')
 const textB = document.getElementById('b')
 const textC = document.getElementById('c')
 const ul = document.querySelector('ul')
+const li = document.querySelector('li')
 
 // O querySelectorAll retorna uma nodelist, enquanto o getElementsByClassName retorna um HTML Collection
 // Com o querySelector, conseguimos usar um for para percorrer sobre os elementos ja que serão nodelist
@@ -75,10 +77,70 @@ console.log(textC.style.background)
 
 // classList
 // mudando o estilo
-bod.classList.add('active', 'red')  // so coloca o nome da class(sem o ponto)
-bod.classList.remove('red')
+ele.classList.add('active', 'red')  // so coloca o nome da class(sem o ponto)
+ele.classList.remove('red')
 
 // o toggle adiciona se não estiver, e remove se estiver
-bod.classList.toggle('red')
-console.log(bod.classList)
+ele.classList.toggle('red')
+ele.classList.remove('red')
+console.log(ele.classList)
+
+// parentElement e parentNode
+/*
+* O parentNode irá pegar o nó pai, ou seja, a tag pai da tag selecionada
+* */
+
+console.log(textC.parentElement)
+console.log(textC.parentNode)
+
+// childNodes ou childrem
+/*
+* Usado para pegar o no filho, ou seja a tag filho da tag selecionada
+* OBS: o childrem elimina os espaços vazios, so trazendo
+*
+* console.log(bod.childNodes)
+* console.log(bod.children)
+*
+* fastchild ou firstElementChild -> pega o primeiro filho ou tag filho
+* firstChild -> leva em consideração o espaço vazio
+* firstElementChild - Não leva em consideração os espaços vazios
+*
+* lastChild ou lastElementChild -> pega o ultimo filho ou tag filho
+* */
+
+console.log(ul.firstChild)
+console.log(ul.firstElementChild)
+/* nextSibling e nextElementSibling
+ * previusSibling e previusElementSibling
+ * OBS: o Element não leva em consideração o espaço vazio
+*
+* */
+console.log(ele.previousElementSibling)
+
+/*
+* Criando e adicionando elementos na pagina
+*
+*
+*/
+
+// createElement - irá criar o elemento
+const div = document.createElement('div');
+div.innerText = 'Minha Div'
+
+// append e prepend
+// append adiciona o item depois da ultima tag
+// body.append(div)
+// prepend adiciona antes da primeira tag
+//body.prepend(div)
+
+/* insertBefore
+* O insertBefore, vai adicionar o elemento antes da tag de referencia
+*
+* Para adicionar depois da tag de referencia, usamos da seguinte maneira
+* precisamos pegar a tag anterior, depois faremos da seguinte forma
+*
+* ele.insertBefore(div, ele.nextElementSibling)
+* */
+body.insertBefore(div, ele.nextElementSibling)
+//body.insertBefore(div, ul)
 
