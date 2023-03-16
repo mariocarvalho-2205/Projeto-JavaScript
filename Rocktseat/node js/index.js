813e1e3a584f045011fa4b267694de50592f7fc2
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-03-16 16:12:07 -0300

    Definição e conceito

---
+++ Rocktseat/node js/index.js
@@ -0,0 +1,45 @@
+/* 
+
+!NodeJS - é uma ferramenta que auxilia no desenvolvimento do: 
+* backend
+* front end
+* micro serviçoes
+* 
+* API - {webapp, mobile, desktop} 
+* 
+* Scripts e automação
+* machine learning
+* I.A - Inteligencia Artificial
+
+! OBS: Não é recomnedado para utilização onde seja exigido muito recurso da CPU (tipo video ou 3d)
+
+Vantagens para se utilizar o nodeJS
+
+Rapidez na Execução e prototipagem
+Alta escalabilidade
+Aplicação de ponta
+JS everywhere
+Ecosistema gigante
+
+! O NodeJS é um JavaScript Runtime Enviroment Ou seja -> Ambiente de Execução
+Ele é responsavél para a comunicação do sistema operacional entre a maquina e o S.O
+
+Google V8 - É o motor V8 que compoe o ambiente , maquina virtual, que ira rodar o JS na maquina
+
+Ele é: 
+* Interpretador de JS para linguagem de maquina
+* Criado em C++
+* Baseado nas ultimas features do JS
+* Focadi oara Chrome mas tem todo o cuidado de não quebrar o Node
+* Não possui o DOM, console ou file
+
+! Node é single-threaded, non-blocking e asynchronous
+
+
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+Variveis globais
+
+
+
+*/
