068a274c5b71de1a6472be2200a35d19647d8643
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-03-20 21:52:58 -0300

    flags

---
+++ Rocktseat/node js/flag.js
@@ -0,0 +1,8 @@
+// sintaxe 
+// node nome_do_arquivo flag "string" flag "string"
+
+// a flag é uma maneira de passar os argumentos podenso ser invertida as suas posições
+
+
+console.log(process.argv)
+
