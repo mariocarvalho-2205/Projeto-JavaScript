1961d6a1ab217033c73e91907975fd40fda316b5
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-03-20 21:19:10 -0300

    descrição

--- Rocktseat/node js/console.js
+++ Rocktseat/node js/console.js
@@ -3,9 +3,17 @@
 
  
  */
-__dirname
-__filename
+// Diretório localn onde está o arquivo que estamos executando
+//__dirname
+global.console.log(__dirname)
+
+// Caminho completo + nome do arquivo 
+//__filename
+global.console.log(__filename)
+
+
+// variavel global
 global.console.log(global)
 
-global.console.log(__filename)
+
 global.console.log("alguma coisa")
