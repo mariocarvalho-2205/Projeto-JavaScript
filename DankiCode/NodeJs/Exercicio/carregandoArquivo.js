e1faba34aaf3d2e126e2df8ff27b5f3f4ea00927
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-03-28 10:35:06 -0300

    exercitando servidores

---
+++ DankiCode/NodeJs/Exercicio/carregandoArquivo.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer(function(req, res) {
+    
+    fs.readFile('index.html', function(err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        res.write(data)
+        return res.end();
+    });
+});
+
+server.listen(port, hostname, function() {
+    console.log('em atividade')
+})
