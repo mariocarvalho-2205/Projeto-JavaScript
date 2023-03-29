b3e37d6947f862b052f9588741fd188653dfedf4
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-03-29 15:41:13 -0300

    atualizando

--- DankiCode/NodeJs/Input_linha_Comando/server.js
+++ DankiCode/NodeJs/Input_linha_Comando/server.js
@@ -10,11 +10,14 @@ const rl = readline.createInterface({
 // aqui ira fazer a leitura e impressao dos dados
 rl.question('Qual o seu nome? ', (nome) => {
     console.log(`Seu nome é ${nome}, NÃO ACREDITOOOO`);
+    rl.question('Qual a sua idade? ', (idade) => {
+        console.log(`Uhauuu ${nome}, você só tem ${idade}`)
+    })
+    
 });
 
-
 // aqui irá responder quando encerrarmos o programa
-rl.on('close', function() {
-    console.log('Adeusssss');
+rl.on('close', function () {
+    console.log(`Parabens!!! Estamos finalizando . Adeusssss!!!`);
     process.exit(0);
 });
