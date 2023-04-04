3ea16cd8612e2c141f430495f11db6e64040f11e
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-04-04 16:06:35 -0300

    Conceitos e principios Programação funcional

---
+++ Rocktseat/Programação_Funcional/prog_funcional.js
@@ -0,0 +1,44 @@
+/*
+
+// Programação Funcional - É um paradigma de programação como a orientação a objetos, por exemplo:
+
+? uma maneira de resolver os problemas
+
+? está envolvido com Funções
+
+* Divide grandes problemas em pequenas tarefas 
+* pequenas tarefas dentro de uma função
+* Abstrair um problema e isolar ele dentro da função
+* Não modificar dados fora dela
+* Pequenas e bem especificas
+
+? Caracteristicas principais da função
+* Um Dado ou mais entra em uma função e um novo dado sai dessa função
+* Não altera dados
+* Não guarda estado / stateless
+
+? Pontos positivos
+* É de facil manutenção
+* É facil uso de testes
+    * Funções isoladas e consistentes
+* Codigos mais confiaveis
+
+// Principios
+
+? Paradigmas
+    * Programação Imperativa
+    * Programação Declarativa
+? Dados
+    * Imutabilidade
+    * Stateless
+? Funções
+    * Independentes
+    * Puras
+    * Higher-order
+    * First-class
+    * Composição
+
+
+
+
+*/
