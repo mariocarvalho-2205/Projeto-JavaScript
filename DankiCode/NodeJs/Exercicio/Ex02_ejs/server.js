9e1edbf89679da3dc341489da2d26c23b48d2370
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-04-06 10:26:09 -0300

    atualizando

--- DankiCode/NodeJs/Exercicio/Ex02_ejs/server.js
+++ DankiCode/NodeJs/Exercicio/Ex02_ejs/server.js
@@ -6,12 +6,28 @@ const path = require('path');
 
 
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
-app.set('views', './views')
+app.set('views', 'views/pages/')
+
+let pessoas = [
+    {
+        nome: 'Mário',
+        idade: 47
+    },
+    {
+        nome: 'Adilma',
+        idade: 48
+    },
+    {
+        nome: 'Joao',
+        idade: 50
+    },
+]
 
 app.get('/', (req, res) => {
-    res.render('index', {nome: 'Mário'})
+    res.render('index', {pessoas: pessoas})
+
 })
 
 app.get('/teste', function (req, res) {
