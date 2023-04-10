3e06a7f8908e6bc75b0395ce8054ebaa53010e6f
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-04-10 16:13:45 -0300

    curso git

--- Rocktseat/Guia Estelar Git/aula_git.js
+++ Rocktseat/Guia Estelar Git/aula_git.js
@@ -37,9 +37,58 @@ Há também, o melhor dos dois mundos, os chamados sistemas distribuídos, algun
 * clientes usam o estado mais recente dos arquivos.
 
 
-O que é o git
-Descrição
-É um sistema de controle de versão distribuído, é open-source, então além de gratuito seu código é aberto, e permite que você volte a qualquer ponto na história do projeto, já que cada alteração no código cria um commit, e cada commit é um ponto na história.
+//O que é o git
+* Descrição
+* É um sistema de controle de versão distribuído, é open-source, então além de gratuito seu código é aberto, e permite que você volte a qualquer ponto na história do projeto, já que cada alteração no código cria um commit, e cada commit é um ponto na história.
 
-Também pode controlar o fluxo de novas funcionalidades, através das ramificações (branch), que funcionam como universos paralelos, onde além do projeto principal, você pode criar uma linha alternativa, adicionar pontos na mesma e depois uni-la novamente à linha principal, sem falar na possibilidade de poder trabalhar com vários devs ao mesmo tempo, analisando e resolvendo conflitos, porém nesse curso não falaremos sobre essas ramificações, teremos mais foco em nossos próprios projetos, as ramificações ficarão para outro curso.
+*Também pode controlar o fluxo de novas funcionalidades, através das ramificações (branch), que funcionam como universos paralelos, onde além do projeto principal, você pode criar uma linha alternativa, adicionar pontos na mesma e depois uni-la novamente à linha principal, sem falar na possibilidade de poder trabalhar com vários devs ao mesmo tempo, analisando e resolvendo conflitos, porém nesse curso não falaremos sobre essas ramificações, teremos mais foco em nossos próprios projetos, as ramificações ficarão para outro curso.
+
+
+//Configuração inicial
+* Descrição
+* Nesse vídeo falaremos sobre a configuração inicial do git, que será feita apenas uma vez por computador e o efeito se manterá mesmo após atualizações, e você também vai poder alterá-las em qualquer momento rodando esses comando novamente* .
+
+?Primeiramente, abra o seu terminal e digite:
+
+* git config --global user.name "Seu nome completo entre aspas"
+* git config --global user.email seuemailaqui@dominio.com
+* Isso é importante porque em cada commit essa informação será utilizada, e é carimbada de forma imutável nos commits que você começa a criar.
+
+! Caso você queira substituir essa informação para um projeto específico, 
+! apenas rode o comando sem a opção --global dentro daquele projeto.
+
+//Caso queira trocar o editor padrão, que é o vim, pelo VS Code, rode o comando abaixo:
+
+* git config --global core.editor "code -w"
+
+?Por fim, caso queira verificar as suas configurações, apenas rode o comando abaixo:
+
+// Ver configurações 
+? para ver as configurações basicas usamos o comando abaixo
+* git config --list
+
+! Para mudarmos algumas das configurações usamos o comando 
+* git config --global item_a_ser_modificado valor
+
+? git config
+* permite ver e atribuir variaveis de configuração como nome e email
+* essas variaveis podem ser armazenadas em 3 lugares diferentes
+    * 1-    /etc/gitconfig: valido para todos os usuarios no sistema e todos os seus repositorios.
+    *       Se você passar a opção --system para git config, ele lê e escreve neste arquivo
+    * 2-    ~/.gitconfig ou ~/.config/git/config: Somente para o seu usuario.
+    *       Você pode fazer o Git ler e escrever neste arquivo passando a opção --global
+    * 3-    config no diretorio Git (ou seja .git/config) de qualquer repositorio qye você esteja
+    *       usando: especifico para este diretorio.
+! Cada nivél sobrescreve os valores no nível especifico, ou seja, valores em .git/config prevalecem sobre
+! /etc/gitconfig.
+
+! NO WINDOWS, O ARQUIVO .gitconfig ESTARÁ NO DIRETORIO $HOME QUE É 
+! C:\Users\$USER
+
+//Git help
+Descrição
+Para termos ajuda com o git, podemos usar o comando git help, onde você vai obeter informações sobre praticamente tudo no git, basta colocar git help + a área (exibida com o comando git help) na qual sua dúvida se encaixa e procurar sua resposta.
+
+Para sair do menu de ajuda, use o comando :q
+
 */
