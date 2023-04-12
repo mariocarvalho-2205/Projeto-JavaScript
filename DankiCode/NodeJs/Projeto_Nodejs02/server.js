b09475689193f363973e6f2809b38073d8f87e00
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-04-12 16:08:11 -0300

    conectando ao banco de dados

--- DankiCode/NodeJs/Projeto_Nodejs02/server.js
+++ DankiCode/NodeJs/Projeto_Nodejs02/server.js
@@ -2,9 +2,19 @@
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
+const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 const path = require('path');
 var bodyParser = require('body-parser');
+
+
+const Posts = require('./Posts.js')
+
+mongoose.connect('mongodb+srv://root:Msct.2205@cluster0.we4or8k.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}).then(function() {
+    console.log('conectado com sucesso')
+}).catch(function(err) {
+    console.log(err.message)
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -22,16 +32,21 @@ app.get('/', (req, res) => {
     //console.log(req.query)  // query vai mostar a busca na url
 
     if(req.query.busca == null) {
+        Posts.find({}).sort({'_id': -1}).exec(function(err, posts) {
+            console.log(posts[0])
+        })
         res.render('home', {})    
     } else {
-        res.send(`Voce buscou por ${req.query.busca}`)
+        res.render('busca', {})
+        // res.send(`Voce buscou por ${req.query.busca}`)
     }
 
     // res.send('home')
 })
 
 app.get('/:slug', (req, res) => {
-    res.send(req.params.slug)
+    res.render('single', {})
+    // res.send(req.params.slug)
 })
 
 
