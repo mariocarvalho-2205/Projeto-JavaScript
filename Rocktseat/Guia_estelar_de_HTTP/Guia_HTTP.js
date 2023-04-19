16ec3723247ef9639632d1b179481c217a836ee3
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-04-19 12:29:46 -0300

    verbo post

--- Rocktseat/Guia_estelar_de_HTTP/Guia_HTTP.js
+++ Rocktseat/Guia_estelar_de_HTTP/Guia_HTTP.js
@@ -248,6 +248,18 @@ curl http://localhost:3000/posts\?q\=json
 * Ele é um método seguro e idempotente, não tem Body nem no envio nem na resposta, não é usado em 
 * formulários e é cacheable.
 
+curl -I http://localhost:3000/posts\?q\=json
+
+// POST
+? Descrição
+* Nesta aula falaremos sobre o método POST, que vem do inglês to post, que serve para publicar ou cadastrar 
+* um recurso. Ele não é seguro nem idempotente, pois muda informações no servidor e não receberá a mesma 
+* resposta de uma mesma requisição, o verbo POST tem Body tanto na requisição quanto na resposta, pode ser 
+* usado em formulários e é cacheable.
+
+curl -d "{}"
+
+
 
 
 */
