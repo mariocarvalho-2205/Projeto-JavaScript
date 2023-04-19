d9bc462a677d61f25c58d7d66c6d476fdb32e1bd
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-04-19 11:47:09 -0300

    methodo options

--- Rocktseat/Guia_estelar_de_HTTP/Guia_HTTP.js
+++ Rocktseat/Guia_estelar_de_HTTP/Guia_HTTP.js
@@ -161,6 +161,13 @@
 * Agora pra iniciar o servidor, podemos usar o comando json-server --watch db.json e assim podemos acessar 
 * esses conteúdos.
 
+
+// OPTIONS
+? Descrição
+* Nesta aula falaremos sobre o método OPTIONS, que é um verbo HTTP que irá nos dar informações sobre a 
+* disponibilidade de métodos da requisição. Ele é um método seguro, pois não faz alteração alguma, e é 
+* idempotente, pois sempre retornará a mesma coisa para a mesma requisição, o OPTIONS não manda nem recebe 
+* um Body, não é usado em formulários e nem é cacheable.
  * 
  * 
  * 
