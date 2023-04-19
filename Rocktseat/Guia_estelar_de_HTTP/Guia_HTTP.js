abb0da679469010238e4633e634fd4059a3b00bc
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-04-19 10:00:45 -0300

    Resource

--- Rocktseat/Guia_estelar_de_HTTP/Guia_HTTP.js
+++ Rocktseat/Guia_estelar_de_HTTP/Guia_HTTP.js
@@ -72,6 +72,11 @@
 * requisições do cliente e mandar uma reposta. A resposta sempre tem um Status Code, e pode ter headers e body.
 
 
+// Resource
+? Descrição
+* Para entendermos como como chegar a um endereço, precisamos entender primeiramente como montar um endereço, 
+* para isso usamos o URI, sigla para Uniform Resource Identifier, Identificador de Recurso Uniforme em português, 
+* que serve para identificar um recurso por seu nome ou sua localização.
 
  * 
  * 
