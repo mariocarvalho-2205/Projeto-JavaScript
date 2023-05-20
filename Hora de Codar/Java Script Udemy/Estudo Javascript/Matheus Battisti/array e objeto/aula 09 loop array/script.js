486cc6278832972a985fa75efabe4b0158ef792e
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-05-20 18:57:31 -0300

    loop array

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/array e objeto/aula 09 loop array/script.js
@@ -0,0 +1,24 @@
+// loop array
+
+let arr = [1, 2, 3, 4, 5, 6, 7]
+
+
+for (let i = 0; i <= arr.length - 1; i++) {
+    console.log(arr[i])
+}
+console.log('-=-=-=-=-=-=-=-=-=')
+let i = 0
+while (i < arr.length) {
+    console.log(arr[i])
+    i++
+}
+
+
+console.log('-=-=-=-=-=-=-=-=-=')
+let j = 0
+do {
+    console.log(arr[j])
+
+    j++
+
+} while( j < arr.length )
