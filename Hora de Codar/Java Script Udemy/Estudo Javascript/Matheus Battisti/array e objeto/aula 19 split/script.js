6235c3d837422767705e90f682eb3bb4fed94cca
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-05-23 15:46:32 -0300

    split

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/array e objeto/aula 19 split/script.js
@@ -0,0 +1,11 @@
+// split
+
+// divide uma string por um separador e retorna um array
+
+let frase = 'O rato roeu a roupa'
+
+let palavras = frase.split(' ')
+
+console.log(frase)
+
+console.log(palavras)
