7a9d108a049b8acb1891db57166888a69618e5b3
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-05-23 15:42:39 -0300

    padStart e padEnd

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/array e objeto/aula 18 padStart/script.js
@@ -0,0 +1,11 @@
+// padStart e padEnd
+
+// padStart - inserir caracteres antes da string
+// padEnd - inserir caracteres depois da string
+
+
+let milContrario = '1'
+
+console.log(milContrario.padStart(4, '0'))
+
+console.log(milContrario.padEnd(4, '0'))
