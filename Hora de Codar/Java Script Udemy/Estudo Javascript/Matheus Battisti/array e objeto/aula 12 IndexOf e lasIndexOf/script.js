1bd20f2f38b8028995077007d0cd76c2bd15e5db
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-05-23 13:37:30 -0300

    indexOf e lastindexOf

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/array e objeto/aula 12 IndexOf e lasIndexOf/script.js
@@ -0,0 +1,17 @@
+// indexOf e lastindexOf
+
+/*
+indexOf - encontra o indice de um determinado elemento
+lastindexOf - encontra o ultimo indice de um elemento
+*/
+
+let numeros = [1, 2, 3, 4, 5, 1, 5, 2, 0]
+
+// retorna o primeiro indice da referencia passada contando do inicio
+console.log(numeros.indexOf(4))  
+
+
+// retorna o ultimo indice da referencia passada
+console.log(numeros.lastIndexOf(1))  
+
+
