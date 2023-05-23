3f2d6d8d76fbbf1b36f3d65870caff68df166c77
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-05-23 13:17:26 -0300

    shift e unshift

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/array e objeto/aula 11 shift e unshift/script.js
@@ -0,0 +1,20 @@
+// shift e unshift
+
+/*
+* shift - remove do inicio do array
+* unshift - adiciona no inicio do array
+
+*/
+
+let frutas = ['maça', 'melao', 'uva']
+console.log(frutas)
+
+let frutaRemovida = frutas.shift()
+
+console.log(frutas)
+
+console.log(frutaRemovida)
+
+frutas.unshift('abacaxi')
+
+console.log(frutas)
