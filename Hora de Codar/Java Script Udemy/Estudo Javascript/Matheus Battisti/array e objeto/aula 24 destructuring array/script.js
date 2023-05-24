ab359ce0accd9ffee6725a6657c2ebfd563edcf5
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-05-24 12:24:19 -0300

    destructuring array

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/array e objeto/aula 24 destructuring array/script.js
@@ -0,0 +1,15 @@
+// destructuring array
+
+/*
+Podemos definir variaveis com os valores de um array 
+utilizando o destruturing
+*/
+
+let nomes =['mario', 'joao', 'maria', 'ady']
+
+let [nomeA, nomeB, nomeC, nomeD] = nomes
+
+console.log(nomeA)
+console.log(nomeB)
+console.log(nomeC)
+console.log(nomeD)
