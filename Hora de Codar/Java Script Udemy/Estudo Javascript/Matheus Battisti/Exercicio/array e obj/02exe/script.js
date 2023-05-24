ecd8e7582d4f2f861d2e43217b13c702be055a3b
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-05-24 13:29:34 -0300

    exe

--- Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/Exercicio/array e obj/02exe/script.js
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/Exercicio/array e obj/02exe/script.js
@@ -3,3 +3,13 @@ let num2 = [5, 6, 7, 8]
 
 console.log(num.length)
 console.log(num2.length)
+
+let onibus = {
+    rodas: 8,
+    passageiros: 40,
+    portas: 2
+}
+
+console.log(onibus.rodas)
+console.log(onibus.passageiros)
+console.log(onibus.portas)
