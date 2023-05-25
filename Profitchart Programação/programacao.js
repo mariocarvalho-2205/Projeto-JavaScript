9c357e827ed153577e89916c5fc7faaba3e730bc
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-05-25 20:32:10 -0300

    profit de venda

--- Profitchart Programação/programacao.js
+++ Profitchart Programação/programacao.js
@@ -18,6 +18,14 @@ begin
             then
                 BuyStop(high+0.02, high+0.02);  // BuyStop = serve para posicionar uma ordem de compra(entrada), 
                                                 // em pontos especificos do grafico
+
+            // Condição de venda
+            if (high[0] > high[1])
+            and (high[0] > high[2])
+            and (close < media(21, close))
+            then
+                SellShortStop(low-0.02, low-0.02) 
+
         end
 
     // bloco de saida da operação
