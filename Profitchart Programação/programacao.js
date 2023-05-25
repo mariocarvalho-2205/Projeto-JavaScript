2db0a83c06cb0f96efb7b7f4ffd20d1c6b31aa28
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-05-25 16:28:36 -0300

    progrmação profit

--- Profitchart Programação/programacao.js
+++ Profitchart Programação/programacao.js
@@ -1,11 +1,13 @@
 /*
-
+Setup Dave Landry
 var
 
     candle: interger;
 
 begin
-    if (BuyPosition = 0) then     // BuyPosition = posição aberta, (0) não tem posição aberta
+
+    // bloco de entrada nas operações
+    if (BuyPosition = 0) and (SellPosition = 0) then     // BuyPosition = posição aberta, (0) não tem posição aberta
         begin
             candle:=0;
 
@@ -17,16 +19,21 @@ begin
                 BuyStop(high+0.02, high+0.02);  // BuyStop = serve para posicionar uma ordem de compra(entrada), 
                                                 // em pontos especificos do grafico
         end
+
+    // bloco de saida da operação
     if(BuyPosition = 1) then
 
         begin
-            candle=candle+1;
+            candle:=candle+1;
             // consição de saida Gain
             // SellToCoverStop = envia uma ordem de venda para sair de uma operação de compra
             
             // Alvo
-            SellToCoverStop(((high-low) * 2));  // verificar o tamanho do cancle de entrada
+            SellToCoverStop((((high[candle]-low[candle]) * 2)+high[candle])+10000,(((high[candle]-low[candle]) * 2)+high[candle]));  // verificar o tamanho do cancle de entrada
 
+
+            // stop
+            SellToCoverStop(abs((high[candle]-low[candle]*2)-low[candle]),abs((high[candle]-low[candle]*2)-low[candle])-10000);
         end;
 end;
 
