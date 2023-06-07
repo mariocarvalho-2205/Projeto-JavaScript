dba49336d535ce7f206838a3d40f4c3749af223c
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-06-06 21:15:10 -0300

    pfr

---
+++ Profitchart Programação/pfr.js
@@ -0,0 +1,24 @@
+/*
+
+var
+
+condicao!, condicao2 : booleano; 
+
+begin
+
+condicao1 := (low[0] < low[1]) and (low[1] < low[2])
+
+condicao2 := (close[0] > close[1])
+
+if (condicao1) and (condicao2) then
+    begin
+        PaintBar(clBlue);
+    end;
+end;
+
+
+
+
+
+
+*/
