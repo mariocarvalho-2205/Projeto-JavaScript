8d5c087de4ec9ab2af682717f340912727faab3d
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-06-12 11:29:17 -0300

    Exercicio Combustivel feito

--- Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/array e objeto/Obj Exe/Exe03 endereços de Cliente/script.js
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/array e objeto/Obj Exe/Exe03 endereços de Cliente/script.js
@@ -1,20 +1,36 @@
-class Automovel {
-    constructor(marca, cor, gasolina) {
-        this.marca = marca
-        this.cor = cor
-        this.gasolina = gasolina
-
+class Endereco {
+    constructor(rua, bairro, cidade, estado) {
+        this.rua = rua
+        this.bairro = bairro
+        this.cidade = cidade
+        this.estado = estado
     }
 
-    dirigir (combustivel) {
-        let cont = 0
-        this.gasolina
+    set novaRua(novaRua) {
+        this.rua = novaRua
+    }
 
+    set novoBairro(novoBairro) {
+        this.bairro = novoBairro
+    }
 
+    set novaCidade(novaCidade) {
+        this.cidade = novaCidade
+    }
+
+    set novoEstado(novoEstado) {
+        this.estado = novoEstado
     }
 }
 
 
-let carro = new Automovel('Ford', 'Preto', 50)
 
-console.log(carro)
+let myEndereco = new Endereco('Aroeira Vermelha', 'Periperi', 'Salvador', 'Bahia')
+
+console.log(myEndereco)
+
+myEndereco.novaRua = 'Rua Daniel Ferreira'
+myEndereco.novoBairro = 'Itacaranha'
+myEndereco.novaCidade = 'Belo Horizonte'
+myEndereco.novoEstado = 'Minas'
+console.log(myEndereco)
