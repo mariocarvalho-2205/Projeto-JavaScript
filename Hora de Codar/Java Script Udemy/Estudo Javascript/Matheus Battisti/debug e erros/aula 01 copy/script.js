3374f6e297676b566daf8e52ef7c48234eafc967
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-06-15 15:28:58 -0300

    Bug eDebug

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/debug e erros/aula 01 copy/script.js
@@ -0,0 +1,11 @@
+let arra = [1, 2, 3, 4, 5, 6]
+let nomes = ['mario', 'ady', 'joao']
+let bool = [true, false, true, false]
+
+console.log(arra[1])
+console.log(nomes[0])
+console.log(bool[2])
+console.log(arra[arra.length - 1])
+console.log(nomes[1].length)
+console.log(nomes[nomes.length - 1])  // acessa o ultimo elemento do array
+console.log(bool.length)
---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/debug e erros/aula 01/script.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const opa = 'teset'
+
+//delete Object.prototype
+
+function teste () {
+    'use strict'
+    let testen = 'agora'
+}
+
+teste()
+
+let obj = {p: 1, p:2}
