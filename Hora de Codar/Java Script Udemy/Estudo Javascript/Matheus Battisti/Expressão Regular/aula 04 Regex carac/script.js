c9f30e04aec7071d844c685ae9bc5abc615f092d
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-07-27 15:00:51 -0300

    REgex praticando

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/Expressão Regular/aula 04 Regex carac/script.js
@@ -0,0 +1,13 @@
+/*
+// Utilizando de varias formas
+* Caracteres Especias
+
+\d - Qualquer tipo de caractere
+\w - Um caractere alfanumerico
+\s - Qualquer caractere de espaço em branco
+\D - Caracteres que não são digitos
+\W - Caractere não alfanumerio
+\S - Caractere quem não seja um espaço em branco
+. - Qualquer caractere, menos nova linha
+
+*/
