423f1dca506f1d309300cadac996a3cbb27da99c
Mario Carvalho <100026592+mariocarvalho-2205@users.noreply.github.com>
2023-07-27 11:27:45 -0300

    Atualizando RegEx

--- Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/Expressão Regular/aula 01 Classe Expressão REgular/script.js
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/Expressão Regular/aula 01 Classe Expressão REgular/script.js
@@ -1,53 +1,19 @@
 /*
-// Classes utilizando o Constructor ES6 
+* Criando Expressão regular
+1 forma de se criar uma expressão regular
+let reg1 = new RegExp("teste");
 
-Com a versão do ES6, uma possibilidade de criar uma classe (Objeto) com
-constructor foi adicionada
+2 forma
+let reg2 = /teste/;
 
-Então não precisamos mais criar por meio de uma função
-
-class Cachorro {
-    constructor(raca) {
-        this.raca = raca
-    }
+* Testando uma expressao regular
+Para testar uma expressao regular, utilizamos o metodo test() para verificar se o padrão é retornado
+console.log(/testando/.test("ttteeesssttaandooo"));
+console.log(/testando/.teste("asdflkjhasd"));
 
 
-}
 
-let labrador = new Cachorro('Labrador')
-
-console.log(labrador.raca)
 */
 
-class Cachorro {
-    constructor(raca, cor) {
-        this.raca = raca
-        this.cor = cor
-    }
-
-    uivar() {
-        console.log('auuuuuu')
-    }
-
-    nome (nome) {
-        return `O nome é ${nome}`;
-    }
-
-
-}
-
-
-// dessa forma adicionamos propriedades atraves da prototype
-
-Cachorro.prototype.rosnar = () => {
-    console.log('grrrrrrr')
-}
-
-let labrador = new Cachorro('Labrador', 'marron')
-
-console.log(labrador.raca)
-labrador.uivar()
-console.log(labrador.nome('mike'))
-labrador.rosnar()
-console.log(labrador.raca, labrador.cor)
-
+let reg1 = new RegExp("testando");
+console.log(/testando/.test(reg1))
