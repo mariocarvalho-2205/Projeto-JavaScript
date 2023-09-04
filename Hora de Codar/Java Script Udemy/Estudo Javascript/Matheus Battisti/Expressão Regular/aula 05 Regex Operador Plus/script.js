85ff8971f54c1bf3ba879aa9e5dc8d516748d51d
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-09-04 15:16:17 -0300

    Operador Plus Adicionado

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/Expressão Regular/aula 05 Regex Operador Plus/script.js
@@ -0,0 +1,16 @@
+/*
+// Utilizando de varias formas
+* Caracteres Especias
+
+\d - Qualquer tipo de caractere
+\w - Um caractere alfanumerico
+\s - Qualquer caractere de espaço em branco
+\D - Caracteres que não são digitos
+\W - Caractere não alfanumerio
+\S - Caractere quem não seja um espaço em branco
+. - Qualquer caractere, menos nova linha
+
+// Operador Plus +
+* Podemos escrever um set que aceite tudo, menos alguns caracteres com o operador NOT ^
+* As combinações serão aceitas, somente será negado se a combinação for combinado com o set
+*/
