65e47d7219ce5b26195752676849fa724a68f70b
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-09-06 14:46:13 -0300

    Exercicio - Regex - Validando Maiusculas

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/Expressão Regular/Exercicio 01 Regex Validando Maiusculas/script.js
@@ -0,0 +1,14 @@
+/*
+// Regex - Exercicio Validando Maiusculas 
+// Utilizando de varias formas
+* Caracteres Especias
+
+\d - Qualquer tipo de caractere não aceita letras e nem vazio, só aceita digitos
+\w - Um caractere alfanumerico
+\s - Qualquer caractere de espaço em branco
+\D - Caracteres que não são digitos
+\W - Caractere não alfanumerio
+\S - Caractere quem não seja um espaço em branco
+. - Qualquer caractere, menos nova linha
+
+*/
