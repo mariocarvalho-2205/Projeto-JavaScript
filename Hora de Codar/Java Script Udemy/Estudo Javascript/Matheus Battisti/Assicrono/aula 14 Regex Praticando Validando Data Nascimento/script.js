e7b4aaa84e2db8b19810d139f64aa86721dd23a2
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-09-09 13:17:02 -0300

    Introdução a Programação assicrona.

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/Assicrono/aula 14 Regex Praticando Validando Data Nascimento/script.js
@@ -0,0 +1,12 @@
+/*
+// Programação Assicrona
+
+É quando uma ação acontence sem seguir necessariamente uma fila
+Ou seja, outras ações podem ser executadas enquanto a assicronia esta sendo executada
+Ex: Usuario está no checkout, manda salvar seu endereço na conta, mas pode prosseguir 
+para a finalização sem recarregar a pagina, pois adicionar endereço ocorre de forma assicrona.
+
+
+
+
+*/
