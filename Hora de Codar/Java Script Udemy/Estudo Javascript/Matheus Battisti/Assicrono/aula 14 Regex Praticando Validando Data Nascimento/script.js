fda50cb4105fe8ac9b5b539d89981d4c615090f6
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-09-09 20:05:01 -0300

    Atualizado nome de pasta programação assicrona

--- Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/Assicrono/aula 14 Regex Praticando Validando Data Nascimento/script.js
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/Assicrono/aula 14 Regex Praticando Validando Data Nascimento/script.js
@@ -6,7 +6,18 @@ Ou seja, outras ações podem ser executadas enquanto a assicronia esta sendo ex
 Ex: Usuario está no checkout, manda salvar seu endereço na conta, mas pode prosseguir 
 para a finalização sem recarregar a pagina, pois adicionar endereço ocorre de forma assicrona.
 
+// Callbacks
+* É uma função que faz uma ação após algum acontecimento no código.
+* Pomdemos utilizar um callback com uma função setTimeout
 
+Ex: 
+console.log("ainda não chamou o callback")
+
+setTimeout(function() {
+    console.log("chamou o callback")
+}, 2000)
+
+console.log("ainda não chamou o callback")
 
 
 */
