b5216d855c313580fc80224d6cb9d7433f07d739
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-09-11 11:50:26 -0300

    JS e o Navegador-Aula 01-Protocolos HTTP, URL/TCP

--- Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/JS e o Navegador/aula 01 Protocolos/script.js
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/JS e o Navegador/aula 01 Protocolos/script.js
@@ -1,23 +1,13 @@
 /*
+// Protocolos
+* Um protocolo é uma forma de comunicação entre computadores através da rede;
+* O HTTP: Serve para nós solicitarmos arquivos, imagens e etc (Hyper Text Transfer Protocol);
+* Entramos em sites graças ao protocolo HTTP;
+! Ao final do protocolo, podemos encontrar o S, que se trata da criptografia de segurança
+! Muito utilizado para logins e senhas
 
-// Generators
+* SMTP = Protocolo para enviar email
+* TCP = Protocolo de transferencia = qudno for realizar transferendcia de arquivos (download)
 
-* Generators funcionam semelhantes as Promises
-* Onde as ações podem ser pausadas e continuadas depois
-* Caracterizando pelo function* e yield
-* O yield pode salvar o estado da variavél
 
 */
-// Ex:
-function* genTest() {
-    let id = 0;
-    while(true) {
-        yield id++
-    }
-}
-
-let criarId = genTest()
-
-console.log(criarId.next().value)
-console.log(criarId.next().value)
-console.log(criarId.next().value)
