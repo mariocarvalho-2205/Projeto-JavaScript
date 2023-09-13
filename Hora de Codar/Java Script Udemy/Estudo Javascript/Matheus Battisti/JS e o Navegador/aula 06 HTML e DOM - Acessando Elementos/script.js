58d8d0f82529d58217930db9f9e385aa1e8009cd
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-09-13 15:13:57 -0300

    HTML e DOM - Acessando Tags

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/JS e o Navegador/aula 06 HTML e DOM - Acessando Elementos/script.js
@@ -0,0 +1,19 @@
+/*
+
+// WEB - HTML e DOM - Document Object Model
+// Acessando Elementos
+
+* Podemos acessar todos os elementos a partir de document.body
+* A partir dele vamos entrando nos childNodes
+* E depois acessando as propriedades que nos interessam
+
+
+
+
+*/
+
+console.log(document.body)
+console.log(document.body.childNodes)
+console.log(document.body.childNodes[1])
+console.log(document.body.childNodes[1].childNodes[1])
+console.log(document.body.childNodes[1].childNodes[1].textContent)
