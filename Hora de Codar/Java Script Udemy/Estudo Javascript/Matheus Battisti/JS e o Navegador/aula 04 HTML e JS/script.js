af2a02d33757bf1c104e22876c274f2f568140a3
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-09-13 14:10:56 -0300

    HTML e JS - Aula 04

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/JS e o Navegador/aula 04 HTML e JS/script.js
@@ -0,0 +1,56 @@
+/*
+
+// WEB - HTML e JavaScript
+
+* Podemos utilizar o Javascript de algumas formas no HTML
+
+* A primeira delas e no proprio corpo do HTML, algumas tags aceitam atributos de script
+* é tambem a menos usada 
+* A segunda forma, é utilizando a tag script no heady do HTML, tambem não é muito 
+* utilizada por tornar a manutenção dificil quando se trata de um projeto muito grande
+* A ultima delas é linkando o javascript a um arquivo externo .js, atraves da tag link com 
+* atributo src, dessa forma podemos deixar o projeto mais organizado, 
+* e de facil manutenção.
+! Lembrando que sempre que linkamos um arquivo externo, estamos chamando ele via protocolo HTML.
+* 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JS e o Navegador - HMTL e JavaScript</title>
+    
+    Exemplo 03 e Mais recompendado pelas boas praticas da programação
+    <script src="script.js"></script>
+
+    <!--
+        Exemplo 02:
+        <script>alert('oi 1')</script>
+    -->
+</head>
+<body>
+    <!-- 
+        Exemplo 01:
+        <script>alert('oi 2')</script>
+     -->
+</body>
+</html>
+* 
+* 
+* 
+* 
+* 
+* 
+* 
+* 
+* 
+
+
+
+
+
+
+
+
+*/
