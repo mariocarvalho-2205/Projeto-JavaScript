6f8e9a19fc43e183c26dc936fa58b8928adb84e4
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-09-13 13:41:52 -0300

    WEB e Js, O HTML - Conceito

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/JS e o Navegador/aula 03 HTML/script.js
@@ -0,0 +1,31 @@
+/*
+
+// WEB - HTML
+
+* HTML ( Hyper Text Markup Language ) é uma linguagem de marcação;
+* Responsavél pelos textos e elementos que vemos na tela ao acessar 
+* uma pagina da web;
+* As tags do HTML criam: titulos paragrafos, listas e etc.
+* Uma tag é definida por <p></p>;
+* Um detalhe importante é que algumas tags não possuem fechamento.
+* Um outro detalhe importante é que, podemos manipular o HTML através do 
+* Javascript, alterando, incluindo ou adicionando elementos, e até mesmo modificando ou
+* adicionando estilos ao css atraves das tags do HTML.
+* Todo documento HTML tem sempre duas partes importantes: head e body
+
+* O heady serve para configurações do documento, como links de folha de estilo (CSS), 
+* Ou até mesmo scripts de JavaScript
+
+* No HTML, no heady(cabeça), é onde esta inclusas as Tags de configuração: meta, title e links.
+
+* O body(corpo) para os elementos (tags), ou seja, a parte visual;
+* Tags do body: p, h1, div, ul, li e etc
+
+
+
+
+
+
+
+
+*/
