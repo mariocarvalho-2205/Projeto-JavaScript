2739f46f55b8c70386ec8446bdb19a6c9fd58f50
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-09-15 12:57:12 -0300

    Aula 07 - Acessando as tags

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/JS e o Navegador/aula 07 HTML e DOM - Encontrando Elementos/script.js
@@ -0,0 +1,24 @@
+/*
+
+// DOM e JS - Encontrando Elementos
+
+
+* Podemos encontrar elementos de uma forma mais facil do que se movendo pelo DOM
+* Vomo HTML temos tags, classes e ids, o javascript nos possibilita utilizar estas 
+* caracteristicas para encontrar o que desejamos.
+* Há alguns metodos para isto:
+* getElementByTagName - Acessa a tag pelo nome -> Sintaxe - document.getElementByTagName('h1)
+
+* getElementById - Acessa a tag pelo ID
+* getElementByClassName - Acessa pela classe
+* tem tambem o 
+* querySelector - Acessa Qualquer uma das opçoes acima, descriminando no parametro
+
+
+
+
+
+
+*/
+// Acessando pela tag
+console.log(document.getElementsByTagName('h2'))
