8be1a35b5298c7cfb64910d4890e216aa6d4f4e7
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-09-19 20:17:21 -0300

    Aula 15 - Incluindo texto ao elemento

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/JS e o Navegador/aula 15 HTML e DOM - Adicionando Elementos Adicionando Texto/script.js
@@ -0,0 +1,21 @@
+/*
+
+// DOM e JS - Adicionando Textos - 
+* Esse metodo repoe um antigo elemento no DOM, com um novo
+
+
+*/
+// Adicionando texto ao elemento
+
+let pSemTexto = document.querySelector('#sem-texto')
+let text = document.createTextNode('texto incluido')
+pSemTexto.appendChild(text)
+
+
+
+
+
+
+
+
+
