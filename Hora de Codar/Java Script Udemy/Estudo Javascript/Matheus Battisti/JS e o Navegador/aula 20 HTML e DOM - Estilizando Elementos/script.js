c2e9906e1de3fb6a80ccd4ea8eeb0da5db02d478
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-09-21 20:22:37 -0300

    Aula 20 - Estilizando o Elemento com JS

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/JS e o Navegador/aula 20 HTML e DOM - Estilizando Elementos/script.js
@@ -0,0 +1,16 @@
+/*
+
+// DOM e JS - Estilizando Elementos 
+* Podemos mudar as propriedades de estilos dos elementos do HTML com o JS
+* Para isso utilizamos a propriedade style e manipulamos o estilo direto
+* na tag, substituindo o do CSS(na maioria das vezes)
+*/
+// Acessando a posição dos elementos
+
+let title = document.getElementById('titulo-principal')
+let tamanho2 = document.getElementById('titulo-principal2')
+
+// Estilizando
+title.style.color = 'blue'
+title.style.border = '1px solid red'
+title.style.padding = '15px'
