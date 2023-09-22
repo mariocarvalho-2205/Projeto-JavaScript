c25f4a3a71404646f005c7a20d8e097907b59e24
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-09-22 17:48:41 -0300

    Criação de pasta para Aulas de Eventos

---
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/Eventos no JS/aula 01 HTML e DOM - Eventos/script.js
@@ -0,0 +1,14 @@
+/*
+
+// DOM e JS - Acessando Varios Elementos
+* Podemos acessar varios elementos utilizando o comando querySelectorAll
+* Eme vez de um podemos acessar todos os elementos semelhantes com o seletor do CSS
+
+
+*/
+// Acessando a posição dos elementos
+
+let title = document.querySelectorAll('p')
+console.log(title)
+
+// Acessando Elementos
