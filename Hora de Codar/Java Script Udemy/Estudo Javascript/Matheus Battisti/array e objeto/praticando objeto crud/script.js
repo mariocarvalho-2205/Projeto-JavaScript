f2daf19d6f738222e4a23bfa1fd5f6186b051d80
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-10-30 15:51:32 -0300

    estudo crud

--- Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/array e objeto/praticando objeto crud/script.js
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/array e objeto/praticando objeto crud/script.js
@@ -39,6 +39,7 @@ class RegistroPessoas {
         // incrementa o id
         this.proximoId++
         this.pessoas.push(pessoa)
+        this.atualizarLista();
     }
 
     // metodo para editar pessoa
@@ -48,6 +49,7 @@ class RegistroPessoas {
         if (pessoaIndex !== -1) {
             this.pessoas[pessoaIndex].setNome(novoNome)
             this.pessoas[pessoaIndex].setIdade(novaIdade)
+            this.atualizarLista();
         } else {
             console.log('pessoa não encontrada')
         }
@@ -57,31 +59,67 @@ class RegistroPessoas {
     apagarPessoa(id) {
         this.pessoas = this.pessoas.filter(p => p.id !== id)
     }
+
+    atualizarLista() {
+        const listaPessoas = document.getElementById('listaPessoas')
+
+        this.pessoas.forEach(pessoa => {
+            const li = document.createElement('li');
+            li.textContent = `ID: ${pessoa.id}, Nome: ${pessoa.nome}, Idade: ${pessoa.idade}, Sexo: ${pessoa.sexo}`
+            listaPessoas.appendChild(li)
+        })
+    }
 }
 
 let registro = new RegistroPessoas()
 
 // Registra pessoa no banco
-registro.adicionarPessoas('mario', 48, 'masculino')
-registro.adicionarPessoas('adilma', 49, 'feminino')
+function adicionarPessoa() {
+    const nomeInput = document.getElementById('nome')
+    const idadeInput = document.getElementById('idade')
+    const sexoInput = document.getElementById('sexo')
 
-console.log(registro.pessoas)
+    registro.adicionarPessoas(nomeInput.value, idadeInput.value, sexoInput.value)
+
+    nomeInput.value = ''
+    idadeInput.value = ''
+    sexoInput.value = ''
+
+}
+
+function editarPessoa( ) {
+    const idInput = prompt('Digite o ID da pessoa que deseja editar:')
+    const nomeInput = prompt('Novo Nome: ')
+    const novaIdade = prompt('Nova Idade:')
+    const novoSexo = prompt('Novo Sexo:')
+
+    registro.editarPessoa(parseInt(idInput), nomeInput, novaIdade, novoSexo)
+}
+
+function apagarPessoa () {
+    const idInput = prompt('Digite o ID da pessoa que deseja apagar:')
+
+    registro.apagarPessoa(parseInt(idInput))
+}
+// registro.adicionarPessoas('adilma', 49, 'feminino')
+
+// console.log(registro.pessoas)
 
 
 // acessa pessoa no banco
 
-registro.pessoas.forEach(index => {
-    if (index.sexo === 'masculino') {
-        console.log(` ${index.nome} tem ${index.idade} anos, e é do sexo ${index.sexo}`)
-    } else {
-        console.log(` ${index.nome} tem ${index.idade} anos, e é do sexo ${index.sexo}`)
-    }
-})
+// registro.pessoas.forEach(index => {
+//     if (index.sexo === 'masculino') {
+//         console.log(` ${index.nome} tem ${index.idade} anos, e é do sexo ${index.sexo}`)
+//     } else {
+//         console.log(` ${index.nome} tem ${index.idade} anos, e é do sexo ${index.sexo}`)
+//     }
+// })
 
 // editando pessoa
-registro.editarPessoa(1, 'mario', 50)
-console.log(registro.pessoas)
+// registro.editarPessoa(1, 'mario', 50)
+// console.log(registro.pessoas)
 
 // apagando pessoa
-registro.apagarPessoa(1)
-console.log(registro.pessoas)
+// registro.apagarPessoa(1)
+// console.log(registro.pessoas)
