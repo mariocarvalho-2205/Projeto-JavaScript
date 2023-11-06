7195a1562ca58e227fa9a582a95debab2ce01df6
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-11-06 11:09:15 -0300

    exercicio de rotas feito

--- Node js/aula 04 - Rotas/rotas.js
+++ Node js/aula 04 - Rotas/rotas.js
@@ -7,11 +7,20 @@ const porta = 3000  // endereço da porta
 // get - recebe 2 parametros para instanciar a rota, 
 // o primeiro e o endereço da rota e o segundo uma callback que ira conter 2 parametros(request e response) que ira exibir na tela o html
 app.get('/', (req, res) => {
-    
     res.send(`<h1>Hello World com Rotas</h1>`)  // envia uma msg e reconhece html
-    // res.end('teste');  // envia uma msg em forma de tela preta, nao reconhece html
-    
+    // res.end('teste');  // envia uma msg em forma de tela preta, nao reconhece html    
 })
+
+
+app.get('/sobre', (req, res) => {
+    res.send(`<h1>Minha pagina Sobre</h1>`)
+})
+
+app.get('/contato', (req, res) => {
+    res.send(`<h1>Minha Rota Contato</h1>`)
+})
+
+
 
 
 
