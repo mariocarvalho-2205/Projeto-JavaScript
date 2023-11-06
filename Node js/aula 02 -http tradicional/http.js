e0573dac12df2dcfd6048c0b0c91d340719f876a
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-11-06 09:31:24 -0300

    atualizando http no node

--- Node js/aula 02 -http tradicional/http.js
+++ Node js/aula 02 -http tradicional/http.js
@@ -3,8 +3,14 @@ let http = require('http')  // modulo tradicional de http no node para criar ser
 
 
 // e necessario abrir um servidor html criando e lendo uma porta
+// createServer - cria um servidor local e recebe uma função de callback com 2 parametros,
+// request e response(requisição e resposta), como resposta, enviamos uma messagem ao html
+// usando o res.end('menssagem')
+// apos a função enviamos o ouvidor com a porta que desejamos utilizar
 http.createServer(function (req, res) {
     res.end('server rodando')
 }).listen(8081)
-
-console.log('Servidor rodando')
+/*
+// toda vez que fizer uma alteração, é necessario encerrar o serviddor com ctrl+c e reiniciar novamente
+ */
+console.log('Servidor rodando')  // aqui enviamos uma mensagem ao terminal informando que esta rodando
