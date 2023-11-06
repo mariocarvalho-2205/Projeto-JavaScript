d82c452d85b50721c8cabe90ee7ae5bc82c68c30
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-11-06 10:50:25 -0300

    server criado com rota

--- Node js/aula 04 - Rotas/rotas.js
+++ Node js/aula 04 - Rotas/rotas.js
@@ -1,5 +1,19 @@
-const express = require('experss')
-const app = express()
-const porta = 3000
+const express = require('express')  // imnportação do express
+const app = express()  // instanciando a função express
+const porta = 3000  // endereço da porta
 
+
+// criação da rota utilizamos get para pegar 
+// get - recebe 2 parametros para instanciar a rota, 
+// o primeiro e o endereço da rota e o segundo uma callback que ira conter 2 parametros(request e response) que ira exibir na tela o html
+app.get('/', (req, res) => {
+    res.end('mensagem vinda do servidor e da rota get /')
+})
+
+
+
+
+
+
+// servidor criado
 app.listen(porta, () => console.log('server rodando, com rotas'))
