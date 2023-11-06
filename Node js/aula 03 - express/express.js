c00e2c5a9398037083e7353df002d9e6ef2504ef
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-11-06 10:31:09 -0300

    criando porta com express

--- Node js/aula 03 - express/express.js
+++ Node js/aula 03 - express/express.js
@@ -1,3 +1,12 @@
 // para realizar a instalação utilizamos o npm
 // antes de instalar e necessario iniciar o npm
 // comando para instalação npm instal express --save
+
+const express = require('express')  // carregando o modulo expess no projeto
+const app = express()  // recebe a função express() criando instancia ou uma copia do express, tornando a principal do sistema
+const PORTA = 3000
+
+
+// criando servidor com express
+// listen(porta) precisa ser a ultima linha do codigo
+app.listen(PORTA, () => console.log(`rodando na porta ${PORTA}`))
