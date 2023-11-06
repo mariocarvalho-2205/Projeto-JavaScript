954e86f9ddbaa30b46d4c9f4cc5ea7d30d59faf8
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-11-06 12:38:52 -0300

    aula 06 nodemon finalizada

--- Node js/aula 06 - Nodemon/lendoHTML.js
+++ Node js/aula 06 - Nodemon/lendoHTML.js
@@ -2,4 +2,8 @@ const express = require('express')
 const app = express()
 const porta = 3000
 
-app.listen()
+app.get('/', (req, res) => {
+    res.send('rodando com nodemon')
+})
+
+app.listen(porta, () => console.log(`porta ${porta} em atividade com nodemon`))
