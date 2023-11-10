e28ebe32d8119e93fb8a245cc9b730e87eb75312
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-11-10 09:48:47 -0300

    mudança porta para 3030

--- Node js/aula 17 - Exibindo dados do mysql/index.js
+++ Node js/aula 17 - Exibindo dados do mysql/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3030
 
 const handlebars = require('express-handlebars')
 const bodyparser = require('body-parser')
