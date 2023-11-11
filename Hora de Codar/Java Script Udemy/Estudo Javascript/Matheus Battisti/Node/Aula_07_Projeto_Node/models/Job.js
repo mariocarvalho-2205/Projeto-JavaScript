bd688362e420da7d33d8ae5a1d5063af58cd29d7
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-11-11 11:44:52 -0300

    modelo criado

--- Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/Node/Aula_07_Projeto_Node/models/Job.js
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/Node/Aula_07_Projeto_Node/models/Job.js
@@ -1,6 +1,26 @@
+const Sequelize = require('sequelize')
 const db = require('../db/db')
 
-const Job = db.sequelize("jobs", {
+const Job = db.define("jobs", {
     // terminar de criar o modelo
-    
+    title: {
+        type: Sequelize.STRING
+    },
+    description: {
+        type: Sequelize.STRING
+    },
+    salary: {
+        type: Sequelize.STRING
+    },
+    company: {
+        type: Sequelize.STRING
+    },
+    email: {
+        type: Sequelize.STRING
+    },
+    new_job: {
+        type: Sequelize.INTEGER
+    }
 })
+
+module.exports = Job
