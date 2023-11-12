34de5963f501a69162d204d5b0e31c69659f7fd8
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-11-12 12:38:08 -0300

    sistema funcionando ate o handlebars

--- Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/Node/Aula_07_Projeto_Node/app.js
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/Node/Aula_07_Projeto_Node/app.js
@@ -1,16 +1,30 @@
 const express    = require('express')
+const handlebars = require('express-handlebars')
 const app        = express()
+const path       = require('path')
 const port       = 3300
 const db         = require('./db/db')
 const bodyParser = require('body-parser')
+
+
+
 
 // body-parser config
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+// handlebars config
+app.set('views', path.join(__dirname, 'views'))
+app.engine('handlebars', handlebars.engine({default: 'main'}))
+app.set('view engine', 'handlebars')
+
+// static folder - configura a pasta central do css e imagens do projeto
+// dessa forma no handlebars, não precisa mais passar a pasta public
+app.use(express.static(path.join(__dirname, 'public')))
+
 // routes
 app.get('/', (req, res) => {
-    res.send('Está funcionando')
+    res.render('index')
 })
 
 // Jobs routes
