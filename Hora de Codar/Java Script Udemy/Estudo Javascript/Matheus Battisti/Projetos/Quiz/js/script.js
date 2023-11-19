0709a82bf0b5748acbb19137e3ac19d1b758641f
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-11-19 17:47:28 -0300

    finalizando

--- Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/Projetos/Quiz/js/script.js
+++ Hora de Codar/Java Script Udemy/Estudo Javascript/Matheus Battisti/Projetos/Quiz/js/script.js
@@ -71,6 +71,83 @@ const questions = [
         },
       ]
     },
-  ] 
+] 
+
+// Substituição do quizz para aprimeira pergunta
+
+function init () {
+    // criar a primeira pergunta
+    createQuestion (0)  // chama a função passando a primeira pergunta pos 0 do array
+}
+
+// função que cria uma pergunta
+function createQuestion (i) {
+
+    // limpar a quastao anterior antes
+    const oldButtons = answerBox.querySelectorAll('button');
+
+    oldButtons.forEach((btn) => {
+        btn.remove()
+    })
+
+    // alterar o texto da pergunta
+    const questionText = document.querySelector('#question-text');
+    const questionNumber = document.querySelector('#question-number');
+
+    questionText.textContent = questions[i].question;
+    questionNumber.textContent = i + 1
+
+    // insere as alternativas
+    questions[i].answers.forEach(function (answer, i) {
+
+        // cria o tamplate do botão quizz
+        const answerTemplate = document.querySelector('.answer-template').cloneNode(true);
+
+        const letterBtn = answerTemplate.querySelector('.btn-letter')
+        const answerText = answerTemplate.querySelector('.question-answer')
+
+        letterBtn.textContent = letters[i]
+        answerText.textContent = answer['answer']
+
+        // atribuindo a classe
+        answerTemplate.setAttribute('correct-answer', answer['correct']);
+
+        // remover hode e template class
+        answerTemplate.classList.remove('hide')
+        answerTemplate.classList.remove('answer-template')
 
 
+        // inserir as alternativas na tela
+        answerBox.appendChild(answerTemplate)
+
+        // inserindo evento de click no botao
+        answerTemplate.addEventListener('click', function() {
+
+            checkAnswer(this)
+            
+        })
+        // console.log(answerTemplate)
+
+    })
+
+    actualQuestion++;
+}
+
+function checkAnswer(btn) {
+    
+
+    const buttons = answerBox.querySelectorAll('button')
+
+    buttons.forEach(function (button) {
+
+        if (button.getAttribute('correct-answer') === 'true') {
+            console.log(button)
+
+        }
+
+    })
+
+}
+
+// inicialização do quizz
+init()
