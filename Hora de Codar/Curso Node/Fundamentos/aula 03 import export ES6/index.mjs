5aac26cd840da5217b32bf3ce14edc3ab8eb0e6b
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-11-19 13:28:50 -0300

    import export ES

---
+++ Hora de Codar/Curso Node/Fundamentos/aula 03 import export ES6/index.mjs
@@ -0,0 +1,3 @@
+import soma from './module.mjs'; // import ES6 mjs
+
+soma(25, 43)
---
+++ Hora de Codar/Curso Node/Fundamentos/aula 03 import export ES6/module.mjs
@@ -0,0 +1,9 @@
+/**
+ * Utilizando import export
+ */
+
+function soma (a,b ) {
+    console.log(a + b)
+}
+
+export default soma;
