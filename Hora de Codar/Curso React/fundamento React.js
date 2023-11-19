6f2a6e9f6c404b86097f973bbfb5953635672473
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-11-19 10:16:43 -0300

    Arquivo Fundamentos criado

---
+++ Hora de Codar/Curso React/fundamento React.js
@@ -0,0 +1,20 @@
+/*
+* Na maioria dos projetos os componentes ficam em uma pasta chamada
+* components, que devemos criar;
+
+* Geralmente são nomeados com a camel case: FirstComponent.js;
+
+* No arquivo criamos uma função, que contém o código deste componente
+* (a lógica e o template);
+
+* E também precisamos exportar esta função, para reutilizá-lo;
+
+// Importando componente
+● A importação é a maneira que temos de reutilizar o componente;
+● Utilizamos a sintaxe: import X from ‘./componentes/X’ onde X é o nome
+do componente;
+● Para colocar o componente importado em outro componente, precisamos
+colocá-lo em forma de tag: <FirstComponent />
+● E então finalizamos o ciclo de importação;
+● Vamos importar o FirstComponent em App;
+*/
