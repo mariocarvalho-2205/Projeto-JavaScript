0e6547daa6b1fa813c3980f1509fc865d4f8bd8f
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-11-23 10:37:17 -0300

    aula jsx finalizada

---
+++ Hora de Codar/Curso React/02-Fundamentos/jsx/src/components/Aside/Aside.jsx
@@ -0,0 +1,9 @@
+const Aside = () => {
+    return (
+        <div className="div-aside">
+            <h1>Meu Aside</h1>
+        </div>
+    )
+}
+
+export default Aside
---
+++ Hora de Codar/Curso React/02-Fundamentos/jsx/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return(
+
+        <div className="div-Header">
+            <h1>Meu Header</h1>
+        </div>
+
+    );
+};
+
+export default Header
